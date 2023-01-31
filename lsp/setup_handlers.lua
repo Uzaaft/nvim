11185e9daf74888f6208c664a0e0e9059b8c6bc3
Micah Halter <micah@mehalter.com>
2023-01-31 15:01:40 -0500

    add fallback for lsp specific plugins

--- lsp/setup_handlers.lua
+++ lsp/setup_handlers.lua
@@ -1,4 +1,9 @@
+local function lsp_plugin_setup(plugin, server, opts)
+  local lsp_plugin_avail, _ = pcall(require, plugin)
+  if not lsp_plugin_avail then require("lspconfig")[server].setup(opts) end
+end
+
 return {
-  tsserver = function(_, opts) require("typescript").setup { server = opts } end,
-  clangd = function(_, opts) require("clangd_extensions").setup { server = opts } end,
+  tsserver = function(...) lsp_plugin_setup("typescript", ...) end,
+  clangd = function(...) lsp_plugin_setup("clangd_extensions", ...) end,
 }
--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   "nanotee/sqls.nvim",
-  "jose-elias-alvarez/typescript.nvim",
-  "p00f/clangd_extensions.nvim",
+  { "jose-elias-alvarez/typescript.nvim", opts = function() return { server = astronvim.lsp.config "tsserver" } end },
+  { "p00f/clangd_extensions.nvim", opts = function() return { server = astronvim.lsp.config "clangd" } end },
   { "neovim/nvim-lspconfig", dependencies = {
     { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
   } },
