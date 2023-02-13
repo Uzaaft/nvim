30065ed2a651aee373b7db5dae450d674dd92719
Micah Halter <micah@mehalter.com>
2023-02-13 16:15:13 -0500

    make tabline not visible by default

--- autocmds.lua
+++ autocmds.lua
@@ -4,13 +4,3 @@ vim.api.nvim_create_autocmd("VimLeave", {
   pattern = "*",
   callback = function() vim.fn.jobstart { "autocomp", vim.fn.expand "%:p", "stop" } end,
 })
-
-vim.api.nvim_create_autocmd("User", {
-  desc = "Hide tabline when only one buffer and one tab",
-  pattern = "AstroBufsUpdated",
-  group = vim.api.nvim_create_augroup("hidetabline", { clear = true }),
-  callback = function()
-    local new_showtabline = #vim.t.bufs > 1 and 2 or 1
-    if new_showtabline ~= vim.opt.showtabline:get() then vim.opt.showtabline = new_showtabline end
-  end,
-})
