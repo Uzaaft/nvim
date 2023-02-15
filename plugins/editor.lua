0ad8a783b7ee9301178ff48bb231b0fe74011d7c
Micah Halter <micah@mehalter.com>
2023-02-15 13:01:44 -0500

    ignore lua_ls root detection

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -69,6 +69,9 @@ return {
   {
     "ahmedkhalf/project.nvim",
     event = "VeryLazy",
+    opts = {
+      ignore_lsp = { "lua_ls" },
+    },
     config = function(_, opts) require("project_nvim").setup(opts) end,
   },
   {
