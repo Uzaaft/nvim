583aba824707672c7048fa75fd49296faa586fdb
Micah Halter <micah@mehalter.com>
2023-02-15 15:31:24 -0500

    use default neo-tree version

--- plugins/core.lua
+++ plugins/core.lua
@@ -1,5 +1,4 @@
 return {
-  { "nvim-neo-tree/neo-tree.nvim", version = false, branch = "main" }, -- use mainline neo-tree for testing new fix
   { "goolord/alpha-nvim", enabled = false },
   { "max397574/better-escape.nvim", enabled = false },
 
