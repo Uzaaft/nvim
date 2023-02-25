ce88c80f062abf47cba34cb62dec36c6ce472f03
Micah Halter <micah@mehalter.com>
2023-02-25 11:24:42 -0500

    update diagnostics format

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -23,18 +23,15 @@ return {
         },
       },
       on_open = function() -- disable diagnostics and indent blankline
-        vim.g.diagnostics_enabled_old = vim.g.diagnostics_enabled
-        vim.g.status_diagnostics_enabled_old = vim.g.status_diagnostics_enabled
-        vim.g.diagnostics_enabled = false
-        vim.g.status_diagnostics_enabled = false
-        vim.diagnostic.config(require("astronvim.utils.lsp").diagnostics["off"])
+        vim.g.diagnostics_mode_old = vim.g.diagnostics_mode
+        vim.g.diagnostics_mode = 0
+        vim.diagnostic.config(require("astronvim.utils.lsp").diagnostics[0])
         vim.g.indent_blankline_enabled_old = vim.g.indent_blankline_enabled
         vim.g.indent_blankline_enabled = false
       end,
       on_close = function() -- restore diagnostics and indent blankline
-        vim.g.diagnostics_enabled = vim.g.diagnostics_enabled_old
-        vim.g.status_diagnostics_enabled = vim.g.status_diagnostics_enabled_old
-        vim.diagnostic.config(require("astronvim.utils.lsp").diagnostics[vim.g.diagnostics_enabled and "on" or "off"])
+        vim.g.diagnostics_mode = vim.g.diagnostics_mode_old
+        vim.diagnostic.config(require("astronvim.utils.lsp").diagnostics[vim.g.diagnostics_mode])
         vim.g.indent_blankline_enabled = vim.g.indent_blankline_enabled_old
       end,
     },
