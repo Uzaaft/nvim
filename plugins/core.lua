6efcf865b4845d7cae1c4852761ffda6a69a6be3
Micah Halter <micah@mehalter.com>
2023-03-13 12:41:30 -0400

    move to line number highlighting for gitsigns

--- plugins/core.lua
+++ plugins/core.lua
@@ -3,5 +3,6 @@ return {
   { "max397574/better-escape.nvim", enabled = false },
 
   { "akinsho/toggleterm.nvim", opts = { terminal_mappings = false } },
+  { "lewis6991/gitsigns.nvim", opts = { signcolumn = false, numhl = true } },
   { "rcarriga/nvim-notify", opts = { timeout = 0 } },
 }
