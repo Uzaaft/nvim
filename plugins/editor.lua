5b73575cb74e7f2ff704ee49c8f745c3d1f09c66
Micah Halter <micah@mehalter.com>
2023-03-29 09:48:38 -0400

    disable project detection for julials

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -64,7 +64,7 @@ return {
     "ahmedkhalf/project.nvim",
     event = "VeryLazy",
     opts = {
-      ignore_lsp = { "lua_ls" },
+      ignore_lsp = { "lua_ls", "julials" },
     },
     config = function(_, opts) require("project_nvim").setup(opts) end,
   },
