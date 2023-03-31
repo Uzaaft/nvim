5eaf34776def650fc305823ba9bee8460c5eee83
Micah Halter <micah@mehalter.com>
2023-03-31 16:45:05 -0400

    change style of current line blame in gitsigns

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,12 +1,14 @@
 return function()
   local get_hlgroup = require("astronvim.utils").get_hlgroup
   local normal = get_hlgroup "Normal"
+  local nontext = get_hlgroup "NonText"
   local fg, bg = normal.fg, normal.bg
   local bg_alt = get_hlgroup("Visual").bg
   local green = get_hlgroup("String").fg
   local red = get_hlgroup("Error").fg
   return {
     CursorLineFold = { link = "CursorLineNr" },
+    GitSignsCurrentLineBlame = { fg = nontext.fg, italic = true },
     HighlightURL = { underline = true },
     TelescopeBorder = { fg = bg_alt, bg = bg },
     TelescopeNormal = { bg = bg },
--- plugins/core.lua
+++ plugins/core.lua
@@ -3,6 +3,9 @@ return {
   { "max397574/better-escape.nvim", enabled = false },
 
   { "akinsho/toggleterm.nvim", opts = { terminal_mappings = false } },
-  { "lewis6991/gitsigns.nvim", opts = { signcolumn = false, numhl = true } },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = { signcolumn = false, numhl = true, current_line_blame_opts = { ignore_whitespace = true } },
+  },
   { "rcarriga/nvim-notify", opts = { timeout = 0 } },
 }
