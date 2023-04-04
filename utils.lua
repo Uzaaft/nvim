b6881b96f4fbab09a0e3f06518e1b70f17a428d6
Micah Halter <micah@mehalter.com>
2023-04-04 10:46:24 -0400

    don't center while searching

--- utils.lua
+++ utils.lua
@@ -53,12 +53,7 @@ function M.better_search(key)
   return function()
     local searched, error =
       pcall(vim.cmd.normal, { args = { (vim.v.count > 0 and vim.v.count or "") .. key }, bang = true })
-    if searched then
-      pcall(vim.cmd.normal, "zzzv")
-    else
-      require("astronvim.utils").notify(error, "error")
-    end
-    vim.opt.hlsearch = searched
+    if not searched then require("astronvim.utils").notify(error, "error") end
   end
 end
 
