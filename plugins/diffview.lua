1d8c2d44be7a7d8a20111679d5cfc8e73fa54e5c
Micah Halter <micah@mehalter.com>
2023-04-05 10:59:19 -0400

    move diffview options to opt instead of config

--- plugins/diffview.lua
+++ plugins/diffview.lua
@@ -1,7 +1,7 @@
 return {
   "sindrets/diffview.nvim",
   event = "User AstroGitFile",
-  config = function()
+  opts = function()
     local actions = require "diffview.actions"
     local utils = require "astronvim.utils" --  astronvim utils
 
@@ -48,7 +48,8 @@ return {
       return out
     end
 
-    require("diffview").setup {
+    return {
+      enhanced_diff_hl = true,
       view = {
         merge_tool = { layout = "diff3_mixed" },
       },
