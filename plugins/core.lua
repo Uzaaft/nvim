6dcb8f56b333e52629bb4d71abdf0aa62492e201
Micah Halter <micah@mehalter.com>
2023-04-13 13:28:37 -0400

    always show gitignore and github folders

--- plugins/core.lua
+++ plugins/core.lua
@@ -2,11 +2,41 @@ return {
   { "goolord/alpha-nvim", enabled = false },
   { "max397574/better-escape.nvim", enabled = false },
 
-  { "akinsho/toggleterm.nvim", opts = { terminal_mappings = false } },
+  {
+    "akinsho/toggleterm.nvim",
+    opts = {
+      terminal_mappings = false,
+    },
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 0,
+    },
+  },
+  {
+    "mrjones2014/smart-splits.nvim",
+    build = "./kitty/install-kittens.bash",
+    opts = {
+      wrap_at_edge = false,
+    },
+  },
   {
     "lewis6991/gitsigns.nvim",
-    opts = { signcolumn = false, numhl = true, current_line_blame_opts = { ignore_whitespace = true } },
+    opts = {
+      signcolumn = false,
+      numhl = true,
+      current_line_blame_opts = { ignore_whitespace = true },
+    },
   },
-  { "rcarriga/nvim-notify", opts = { timeout = 0 } },
-  { "mrjones2014/smart-splits.nvim", build = "./kitty/install-kittens.bash", opts = { wrap_at_edge = false } },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          always_show = { ".github", ".gitignore" },
+        },
+      },
+    },
+  },
 }
