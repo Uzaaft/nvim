cfeeff2c0cae604ce213872416d9c12c6212ebaa
Micah Halter <micah@mehalter.com>
2023-05-19 09:58:13 -0400

    add rome lsp

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -15,6 +15,7 @@ return {
         "julials",
         "pyright",
         "lua_ls",
+        "rome",
         "taplo",
         "texlab",
         "tsserver",
@@ -36,6 +37,20 @@ return {
       },
       handlers = {
         taplo = function() end, -- disable taplo in null-ls, it's taken care of by lspconfig
+        rome = function() end, -- disable rome in null-ls, it's taken care of by lspconfig
+        prettierd = function()
+          local null_ls = require "null-ls"
+          null_ls.register(null_ls.builtins.formatting.prettierd.with {
+            disabled_filetypes = {
+              "javascript",
+              "javascriptreact",
+              "json",
+              "typescript",
+              "typescript.tsx",
+              "typescriptreact",
+            },
+          })
+        end,
       },
     },
   },
