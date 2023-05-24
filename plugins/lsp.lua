9e3c1e9bc859111775ed828fce4cf45f942881b6
Micah Halter <micah@mehalter.com>
2023-05-24 07:52:17 -0400

    add lsp-inlayhints back

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -1,5 +1,29 @@
 return {
   {
+    "lvimuser/lsp-inlayhints.nvim",
+    init = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("LspAttach_inlayhints", {}),
+        callback = function(args)
+          if not (args.data and args.data.client_id) then return end
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client.server_capabilities.inlayHintProvider then
+            local inlayhints = require "lsp-inlayhints"
+            inlayhints.on_attach(client, args.buf)
+            require("astronvim.utils").set_mappings({
+              n = {
+                ["<leader>uH"] = { inlayhints.toggle, desc = "Toggle inlay hints" },
+              },
+            }, { buffer = args.buf })
+          end
+        end,
+      })
+    end,
+    opts = {
+      inlay_hints = { highlight = "Comment" },
+    },
+  },
+  {
     "jose-elias-alvarez/typescript.nvim",
     init = function() table.insert(astronvim.lsp.skip_setup, "tsserver") end,
     ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
@@ -9,7 +33,12 @@ return {
     "p00f/clangd_extensions.nvim",
     init = function() table.insert(astronvim.lsp.skip_setup, "clangd") end,
     ft = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-    opts = function() return { server = require("astronvim.utils.lsp").config "clangd" } end,
+    opts = function()
+      return {
+        server = require("astronvim.utils.lsp").config "clangd",
+        extensions = { autoSetHints = false },
+      }
+    end,
   },
   { "neovim/nvim-lspconfig", dependencies = {
     { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
