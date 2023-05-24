84dc734c71a31d027c1d68909055bcfa49c266a8
Micah Halter <micah@mehalter.com>
2023-05-24 12:10:29 -0400

    add more details to autocmds

--- autocmds.lua
+++ autocmds.lua
@@ -1,25 +1,24 @@
--- auto stop auto-compiler if its running
 vim.api.nvim_create_autocmd("VimLeave", {
-  desc = "Stop running auto compiler",
-  group = vim.api.nvim_create_augroup("autocomp", { clear = true }),
+  desc = "Stop running auto compiler on leave",
+  group = vim.api.nvim_create_augroup("quit_autocomp", { clear = true }),
   pattern = "*",
   callback = function() vim.fn.jobstart { "autocomp", vim.fn.expand "%:p", "stop" } end,
 })
 
--- text like documents enable wrap and spell
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "gitcommit", "markdown", "text", "plaintex" },
+  desc = "Enable wrap and spell for text like documents",
   group = vim.api.nvim_create_augroup("auto_spell", { clear = true }),
+  pattern = { "gitcommit", "markdown", "text", "plaintex" },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
   end,
 })
 
--- auto hide tabline
 vim.api.nvim_create_autocmd("User", {
+  desc = "Auto hide tabline",
+  group = vim.api.nvim_create_augroup("autohide_tabline", { clear = true }),
   pattern = "AstroBufsUpdated",
-  group = vim.api.nvim_create_augroup("autohidetabline", { clear = true }),
   callback = function()
     local new_showtabline = #vim.t.bufs > 1 and 2 or 1
     if new_showtabline ~= vim.opt.showtabline:get() then vim.opt.showtabline = new_showtabline end
