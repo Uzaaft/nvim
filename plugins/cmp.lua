323d927a6e0fda1a4935dd4b96319d281686279b
Micah Halter <micah@mehalter.com>
2023-05-25 12:28:23 -0400

    revamp cmp mappings

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -10,12 +10,9 @@ return {
     local cmp = require "cmp"
     local luasnip = require "luasnip"
 
-    local function next_item()
-      if cmp.visible() then
-        cmp.select_next_item { behavior = cmp.SelectBehavior.Insert }
-      else
-        cmp.complete()
-      end
+    local function has_words_before()
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
     end
 
     return require("astronvim.utils").extend_tbl(opts, {
@@ -37,23 +34,31 @@ return {
         { name = "buffer", priority = 250 },
       },
       mapping = {
-        ["<C-n>"] = next_item,
-        ["<C-j>"] = next_item,
+        -- tab complete
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.jumpable(1) then
-            luasnip.jump(1)
+          if cmp.visible() and has_words_before() then
+            cmp.confirm { select = true }
           else
             fallback()
           end
         end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if luasnip.jumpable(-1) then
-            luasnip.jump(-1)
+        -- <C-n> and <C-p> for navigating snippets
+        ["<C-n>"] = function()
+          if luasnip.jumpable(1) then luasnip.jump(1) end
+        end,
+        ["<C-p>"] = function()
+          if luasnip.jumpable(-1) then luasnip.jump(-1) end
+        end,
+        -- <C-j> for starting completion
+        ["<C-j>"] = function()
+          if cmp.visible() then
+            cmp.select_next_item { behavior = cmp.SelectBehavior.Insert }
           else
-            fallback()
+            cmp.complete()
           end
-        end, { "i", "s" }),
+        end,
       },
+      experimental = { ghost_text = true },
     })
   end,
 }
