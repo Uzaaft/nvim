6fd06869d8cff6b278acb69069b4ce9e6e80baaf
Micah Halter <micah@mehalter.com>
2023-06-22 15:07:11 -0400

    add back nvim-ts-rainbow2

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
     { "andymass/vim-matchup", init = function() vim.g.matchup_matchparen_deferred = 1 end },
+    { "HiPhish/nvim-ts-rainbow2" },
   },
   opts = {
     auto_install = vim.fn.executable "tree-sitter" == 1,
