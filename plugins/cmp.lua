48a999a955e713ea8be060f8eda99604ee4c72fe
Micah Halter <micah@mehalter.com>
2023-06-27 07:57:03 -0400

    improve cmp sorting

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -36,10 +36,24 @@ return {
       },
       sorting = {
         comparators = {
-          compare.locality,
-          compare.recently_used,
-          compare.score,
           compare.offset,
+          compare.exact,
+          compare.score,
+          compare.recently_used,
+          function(entry1, entry2)
+            local _, entry1_under = entry1.completion_item.label:find "^_+"
+            local _, entry2_under = entry2.completion_item.label:find "^_+"
+            entry1_under = entry1_under or 0
+            entry2_under = entry2_under or 0
+            if entry1_under > entry2_under then
+              return false
+            elseif entry1_under < entry2_under then
+              return true
+            end
+          end,
+          compare.kind,
+          compare.sort_text,
+          compare.length,
           compare.order,
         },
       },
