0c453d3cea0ae3d279180afbb827e7101ad06687
Micah Halter <micah@mehalter.com>
2023-07-13 14:30:16 -0400

    remove netman

--- plugins/core.lua
+++ plugins/core.lua
@@ -29,20 +29,7 @@ return {
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
-    dependencies = { "miversen33/netman.nvim" },
     opts = {
-      sources = {
-        "filesystem",
-        "netman.ui.neo-tree",
-        "git_status",
-      },
-      source_selector = {
-        sources = {
-          { source = "filesystem", display_name = get_icon "FolderClosed" .. " File" },
-          { source = "remote", display_name = "󰒍 Remote" },
-          { source = "git_status", display_name = get_icon "Git" .. " Git" },
-        },
-      },
       filesystem = {
         hijack_netrw_behavior = "open_default",
         filtered_items = {
