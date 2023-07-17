00c9486f40577b46a53089aa55d588e6394bc409
Micah Halter <micah@mehalter.com>
2023-07-17 06:42:43 -0400

    reorder noice filters

--- plugins/noice.lua
+++ plugins/noice.lua
@@ -23,11 +23,11 @@ return {
         signature = { enabled = false },
       },
       routes = {
+        { filter = { event = "msg_show", min_height = 20 }, view = "messages" }, -- send long messages to split
         { filter = { event = "msg_show", find = "%d+L,%s%d+B" }, opts = { skip = true } }, -- skip save notifications
         { filter = { event = "msg_show", find = "^%d+ more lines$" }, opts = { skip = true } }, -- skip paste notifications
         { filter = { event = "msg_show", find = "^%d+ fewer lines$" }, opts = { skip = true } }, -- skip delete notifications
         { filter = { event = "msg_show", find = "^%d+ lines yanked$" }, opts = { skip = true } }, -- skip yank notifications
-        { filter = { event = "msg_show", min_height = 20 }, view = "messages" }, -- send long messages to split
       },
     },
   },
