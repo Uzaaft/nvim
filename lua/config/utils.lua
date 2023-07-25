683875486d0bfe8c92fa1cea118f1051a7c5cafe
Micah Halter <micah@mehalter.com>
2023-07-25 15:27:21 -0400

    simplify mappings

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -1,15 +1,9 @@
 local M = {}
 
 function M.vim_opt_toggle(opt, on, off, name)
-  if on == nil then
-    on = true
-  end
-  if off == nil then
-    off = false
-  end
-  if not name then
-    name = opt
-  end
+  if on == nil then on = true end
+  if off == nil then off = false end
+  if not name then name = opt end
   local is_off = vim.opt[opt]:get() == off
   vim.opt[opt] = is_off and on or off
   require("astrocore.utils").notify(name .. " " .. (is_off and "Enabled" or "Disabled"))
@@ -19,9 +13,7 @@ function M.async_run(cmd, on_finish)
   local lines = { "" }
 
   local function on_event(_, data, event)
-    if (event == "stdout" or event == "stderr") and data then
-      vim.list_extend(lines, data)
-    end
+    if (event == "stdout" or event == "stderr") and data then vim.list_extend(lines, data) end
 
     if event == "exit" then
       vim.fn.setqflist({}, " ", {
@@ -29,9 +21,7 @@ function M.async_run(cmd, on_finish)
         lines = lines,
         efm = "%f:%l:%c: %t%n %m",
       })
-      if on_finish then
-        on_finish()
-      end
+      if on_finish then on_finish() end
     end
   end
 
@@ -60,13 +50,9 @@ function M.toggle_qf()
 end
 
 function M.better_search(key)
-  return function()
-    local searched, error =
-      pcall(vim.cmd.normal, { args = { (vim.v.count > 0 and vim.v.count or "") .. key }, bang = true })
-    if not searched and type(error) == "string" then
-      require("astrocore.utils").notify(error, vim.log.levels.ERROR)
-    end
-  end
+  local searched, error =
+    pcall(vim.cmd.normal, { args = { (vim.v.count > 0 and vim.v.count or "") .. key }, bang = true })
+  if not searched and type(error) == "string" then require("astrocore.utils").notify(error, vim.log.levels.ERROR) end
 end
 
 return M
