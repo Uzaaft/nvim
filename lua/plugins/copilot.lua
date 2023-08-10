44bb59b42f8e5be8dc6e0cf5eec9b7269e9c1b18
Uzair Aftab <uzaaft@outlook.com>
2023-08-10 10:01:41 +0200

    feat: Make copilot mappings compatible with my term

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = {
+      keymap = {
+        accept = "<C-l>",
+        accept_word = false,
+        accept_line = false,
+        next = "<C-.>",
+        prev = "<C-,>",
+        dismiss = "<C/>",
+        --dismiss = false,
+      },
+    },
+  },
+}
