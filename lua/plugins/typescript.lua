3113ce84b976d31a295f930738bcff9f414b555e
Uzair Aftab <uzaaft@outlook.com>
2023-08-12 15:35:15 +0200

    feat: Add typescript support

---
+++ lua/plugins/typescript.lua
@@ -0,0 +1,22 @@
+return {
+  "pmizio/typescript-tools.nvim",
+  ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  opts = function()
+    return require("astrocore").extend_tbl(require("astrolsp").lsp_opts "tsserver", {
+      settings = {
+        tsserver_path = require("mason-registry").get_package("typescript-language-server"):get_install_path()
+          .. "/node_modules/typescript/lib/tsserver.js",
+        tsserver_file_preferences = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = "all",
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+        },
+      },
+    })
+  end,
+}
