efd096a000db7d7ab9acd00cb2565a6c67f7b834
Micah Halter <micah@mehalter.com>
2023-08-14 08:17:37 -0400

    feat(cmp): move back to AstroNvim default super-tab

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,12 +9,6 @@ return {
   opts = function(_, opts)
     local cmp = require "cmp"
     local compare = require "cmp.config.compare"
-    local luasnip = require "luasnip"
-
-    local function has_words_before()
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-    end
 
     return require("astrocore").extend_tbl(opts, {
       window = { completion = { col_offset = -1, side_padding = 0 } },
@@ -50,31 +44,6 @@ return {
           compare.length,
           compare.order,
         },
-      },
-      mapping = {
-        -- tab complete
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() and has_words_before() then
-            cmp.confirm { select = true }
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        -- <C-n> and <C-p> for navigating snippets
-        ["<C-n>"] = cmp.mapping(function()
-          if luasnip.jumpable(1) then luasnip.jump(1) end
-        end, { "i", "s" }),
-        ["<C-p>"] = cmp.mapping(function()
-          if luasnip.jumpable(-1) then luasnip.jump(-1) end
-        end, { "i", "s" }),
-        -- <C-j> for starting completion
-        ["<C-j>"] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_next_item { behavior = cmp.SelectBehavior.Insert }
-          else
-            cmp.complete()
-          end
-        end, { "i", "s" }),
       },
     })
   end,
