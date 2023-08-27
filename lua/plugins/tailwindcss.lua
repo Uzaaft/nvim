c80ed673a5dc29b229a7b0acaabfbb4e265a4d97
Uzair Aftab <uzaaft@outlook.com>
2023-08-27 20:57:38 +0200

    feat: Tailwindcss stuff

---
+++ lua/plugins/tailwindcss.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "js-everts/cmp-tailwind-colors", opts = {} },
+    },
+    opts = function(_, opts)
+      local format_kinds = opts.formatting.format
+      opts.formatting.format = function(entry, item)
+        if item.kind == "Color" then
+          item = require("cmp-tailwind-colors").format(entry, item)
+          if item.kind == "Color" then return format_kinds(entry, item) end
+          return item
+        end
+        return format_kinds(entry, item)
+      end
+    end,
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        names = true,
+        tailwind = true,
+      },
+    },
+  },
+}
