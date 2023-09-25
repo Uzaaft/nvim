19c0d1de40a6eb30bb415ba8fc1cd0ad310ae0bf
Uzair Aftab <uzaaft@outlook.com>
2023-09-25 17:41:03 +0200

    chore: cleanup

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -7,14 +7,6 @@ return {
     },
   },
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "rust")
-      end
-    end,
-  },
-  {
     "simrat39/rust-tools.nvim",
     ft = { "rust" },
     opts = function()
@@ -38,13 +30,8 @@ return {
     dependencies = {
       {
         "jay-babu/mason-nvim-dap.nvim",
-        opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "codelldb") end,
       },
     },
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "rust_analyzer") end,
   },
   {
     "Saecki/crates.nvim",
