8f269519687277695976572f92d9b76db9e72352
Uzair Aftab <uzaaft@outlook.com>
2023-10-08 13:00:02 +0200

    fix: misc fixes, cleanup, and auto install a few key things

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,20 +7,18 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "taplo",
+        "rust_analyzer",
       })
     end,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
     opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
-      })
+      -- Ensure that opts.handlers exists and is a table
+      if not opts.handlers then opts.handlers = {} end
+      opts.handlers.taplo = function() end
     end,
   },
   {
@@ -30,12 +28,14 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         -- "python",
+        "codelldb",
       })
     end,
   },
   {
     "williamboman/mason.nvim",
     opts = {
+      PATH = "append",
       registries = {
         "lua:custom-registry",
         "github:mason-org/mason-registry",
