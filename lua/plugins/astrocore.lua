34bd86f844ed37309201bbc90b34528c80a7827e
Uzair Aftab <uzaaft@outlook.com>
2023-10-23 08:03:09 +0200

    chore: Some misc astrocore default stuff

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,12 @@
 ---@type AstroCoreOpts
 local opts = {
+  features = {
+    max_file = { size = 1024 * 100, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+    autopairs = true, -- enable autopairs at start
+    cmp = true, -- enable completion at start
+    highlighturl = true, -- highlight URLs at start
+    notifications = true, -- enable notifications at start
+  },
   autocmds = {
     auto_spell = {
       {
