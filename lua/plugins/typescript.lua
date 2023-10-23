7b5c0d3dd7e7e975ad0f1ed79102de93f7f6b0b2
Uzair Aftab <uzaaft@outlook.com>
2023-10-23 08:05:58 +0200

    fix: Disable tsserver handler

--- lua/plugins/typescript.lua
+++ lua/plugins/typescript.lua
@@ -1,6 +1,10 @@
 return {
   "pmizio/typescript-tools.nvim",
   ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  dependencies = {
+    "AstroNvim/astrolsp",
+    opts = { handlers = { tsserver = false } },
+  },
   opts = function()
     return require("astrocore").extend_tbl(require("astrolsp").lsp_opts "tsserver", {
       settings = {
