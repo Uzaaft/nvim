3e0c1c5651f3ce127aa0f72684a7e3f7a3209c3a
Micah Halter <micah@mehalter.com>
2023-10-24 16:14:37 -0400

    fix(neogit): remove lingering fzy sorter reference

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -16,7 +16,6 @@ return {
     local fold_signs = { get_icon "FoldClosed", get_icon "FoldOpened" }
     return {
       disable_builtin_notifications = true,
-      telescope_sorter = function() return require("telescope").extensions.fzy_native.native_fzy_sorter() end,
       integrations = { telescope = true },
       signs = { section = fold_signs, item = fold_signs },
     }
