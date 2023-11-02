1c829cca4b9e32818f8aaebd4f61d3a7074c8411
Micah Halter <micah@mehalter.com>
2023-11-02 13:57:07 -0400

    fix(nvim-lint): allow linters to not be installed

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -10,9 +10,35 @@ return {
   },
   config = function(_, opts)
     local lint = require "lint"
-    lint.linters_by_ft = opts.linters_by_ft
 
-    lint.try_lint() -- start linter immediately
+    lint.linters_by_ft = opts.linters_by_ft
+    for name, linter in pairs(opts.linters or {}) do
+      lint.linters[name] = (type(linter) == "table" and type(lint.linters[name]) == "table")
+          and vim.tbl_deep_extend("force", lint.linters[name], linter)
+        or linter
+    end
+
+    local function try_lint()
+      local names = lint._resolve_linter_by_ft(vim.bo.filetype)
+
+      -- Add fallback linters and global linters.
+      if #names == 0 then names = lint.linters_by_ft["_"] or {} end
+      vim.list_extend(names, lint.linters_by_ft["*"] or {})
+
+      -- Filter out linters that don't exist or don't match the condition.
+      local ctx = { filename = vim.api.nvim_buf_get_name(0) }
+      ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
+      names = vim.tbl_filter(function(name)
+        local linter = lint.linters[name]
+        return linter
+          and vim.fn.executable(linter.cmd) == 1
+          and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
+      end, names)
+
+      lint.try_lint(names)
+    end
+
+    try_lint() -- start linter immediately
     local timer = vim.loop.new_timer()
     vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged" }, {
       group = vim.api.nvim_create_augroup("auto_lint", { clear = true }),
@@ -20,7 +46,7 @@ return {
       callback = function()
         timer:start(100, 0, function()
           timer:stop()
-          vim.schedule(lint.try_lint)
+          vim.schedule(try_lint)
         end)
       end,
     })
