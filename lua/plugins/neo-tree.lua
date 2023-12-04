c9d782985ea37199b870869d3ff2929b0978b206
Micah Halter <micah.halter@gtri.gatech.edu>
2023-12-04 18:27:13 -0500

    feat(oil): use `oil` for directory hijacking

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,7 +2,6 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
-      hijack_netrw_behavior = "open_default",
       filtered_items = { always_show = { ".github", ".gitignore" } },
     },
     nesting_rules = {
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,11 +4,35 @@ return {
   "mehalter/oil.nvim",
   branch = "sessionloadpost_perf",
   cmd = "Oil",
+  init = function() -- start oil on startup lazily if necessary
+    if vim.fn.argc() == 1 then
+      local arg = vim.fn.argv(0)
+      local stat = vim.loop.fs_stat(arg)
+      local adapter = string.match(arg, "^([%l-]*)://")
+      if (stat and stat.type == "directory") or adapter == "oil-ssh" then require "oil" end
+    end
+  end,
   dependencies = {
+    { "nvim-neo-tree/neo-tree.nvim", opts = { filesystem = { hijack_netrw_behavior = "disabled" } } },
     {
       "AstroNvim/astrocore",
       opts = {
         autocmds = {
+          neotree_start = false,
+          oil_start = {
+            {
+              event = "BufNew",
+              desc = "start oil when editing a directory",
+              callback = function()
+                if package.loaded["oil"] then
+                  vim.api.nvim_del_augroup_by_name "oil_start"
+                elseif vim.fn.isdirectory(vim.fn.expand "<afile>") == 1 then
+                  require "oil"
+                  vim.api.nvim_del_augroup_by_name "oil_start"
+                end
+              end,
+            },
+          },
           oil_settings = {
             {
               event = "FileType",
