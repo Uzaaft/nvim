2829f17ad2d828cd5100aa83d5012e65dcbe838d
Uzair Aftab <uzaaft@outlook.com>
2023-12-05 09:18:33 +0100

    fix: fix cmp-line

--- lua/plugins/cmp-line.lua
+++ lua/plugins/cmp-line.lua
@@ -1,22 +1,33 @@
-local cmp = require "cmp"
-
-return {
-  "hrsh7th/cmp-cmdline",
-  event = { "CmdlineEnter" },
-  opts = {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = "path" },
-    }, {
-      {
-        name = "cmdline",
-        option = {
-          ignore_cmds = { "Man", "!" },
-        },
-      },
-    }, {
-      { name = "buffer" },
-    }),
+return { -- override nvim-cmp plugin
+  "hrsh7th/nvim-cmp",
+  keys = { ":", "/", "?" }, -- lazy load cmp on more keys along with insert mode
+  dependencies = {
+    "hrsh7th/cmp-cmdline", -- add cmp-cmdline as dependency of cmp
   },
-  config = function(_, opts) require("cmp").setup.cmdline(":", opts) end,
+  config = function(_, opts)
+    local cmp = require "cmp"
+    -- run cmp setup
+    cmp.setup(opts)
+
+    -- configure `cmp-cmdline` as described in their repo: https://github.com/hrsh7th/cmp-cmdline#setup
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        {
+          name = "cmdline",
+          option = {
+            ignore_cmds = { "Man", "!" },
+          },
+        },
+      }),
+    })
+  end,
 }
