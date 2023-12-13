748f301adf654cdf28f5e1f61f35a2530c10925c
Micah Halter <micah@mehalter.com>
2023-12-13 11:14:11 -0500

    feat: add some basic vscode neovim integration

---
+++ lua/plugins/vscode.lua
@@ -0,0 +1,36 @@
+if not vim.g.vscode then return {} end
+
+local enabled = {
+  "lazy.nvim",
+  "Comment.nvim",
+  "nvim-autopairs",
+  "nvim-treesitter",
+  "nvim-ts-autotag",
+  "nvim-treesitter-textobjects",
+  "nvim-ts-context-commentstring",
+  "astrocore",
+  "astroui",
+}
+
+local Config = require "lazy.core.config"
+Config.options.checker.enabled = false
+Config.options.change_detection.enabled = false
+Config.options.defaults.cond = function(plugin) return vim.tbl_contains(enabled, plugin.name) or plugin.vscode end
+
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>ff"] = "<CMD>Find<CR>",
+          ["<Leader>fw"] = "<CMD>call VSCodeNotify('workbench.action.findInFiles')<CR>",
+          ["<Leader>ls"] = "<CMD>call VSCodeNotify('workbench.action.gotoSymbol')<CR>",
+        },
+      },
+    },
+  },
+  { "AstroNvim/astroui", opts = { colorscheme = false } },
+  { "nvim-treesitter/nvim-treesitter", opts = { highlight = { enable = false } } },
+}
