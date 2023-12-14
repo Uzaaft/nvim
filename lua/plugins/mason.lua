668e9e50af183d2a40d6057b2723f79377cc3974
Micah Halter <micah@mehalter.com>
2023-12-14 09:44:09 -0500

    fix(mason): add type safety to mason registry modfication

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,9 +1,9 @@
 return {
   "williamboman/mason.nvim",
-  opts = {
-    registries = {
-      "lua:custom-registry",
-      "github:mason-org/mason-registry",
-    },
-  },
+  opts = function(_, opts)
+    opts.registries = require("astrocore").list_insert_unique(
+      opts.registries,
+      { "lua:custom-registry", "github:mason-org/mason-registry" }
+    )
+  end,
 }
