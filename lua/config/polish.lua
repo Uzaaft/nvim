819583779e005c7557aa04b322c7eebac9650ffe
Uzair Aftab <uzaaft@outlook.com>
2023-12-16 17:42:51 +0100

    feat: remove unneeded code

--- lua/config/polish.lua
+++ lua/config/polish.lua
@@ -14,13 +14,6 @@ vim.filetype.add {
 
 local inlay_hints_group = vim.api.nvim_create_augroup("uzaaft/toggle_inlay_hints", { clear = false })
 
--- Initial inlay hint display.
--- Idk why but without the delay inlay hints aren't displayed at the very start.
-vim.defer_fn(function()
-  local mode = vim.api.nvim_get_mode().mode
-  vim.lsp.inlay_hint.enable(bufnr, mode == "n" or mode == "v")
-end, 500)
-
 vim.api.nvim_create_autocmd("InsertEnter", {
   group = inlay_hints_group,
   desc = "Enable inlay hints",
