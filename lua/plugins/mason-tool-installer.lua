38be17e9b2816f59954d312cc0583fab6bf4d43f
Uzair Aftab <uzaaft@outlook.com>
2023-12-17 13:22:37 +0100

    feat: yoink mehalters commit

--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -1,53 +1,73 @@
 return {
-  "WhoIsSethDaniel/mason-tool-installer.nvim",
-  cmd = { "MasonToolsInstall", "MasonToolsUpdate", "MasonToolsClean" },
-  dependencies = { "williamboman/mason.nvim" },
-  init = function()
-    require("astrocore").on_load("mason.nvim", function() require "mason-tool-installer" end)
-  end,
-  opts = {
-    ensure_installed = {
-      -- Language Servers
-      "ansiblels",
-      "clangd",
-      "cssls",
-      "gopls",
-      "hls",
-      "html",
-      "intelephense",
-      "marksman",
-      "neocmake",
-      "jsonls",
-      "julials",
-      "lua_ls",
-      "taplo",
-      "texlab",
-      "tsserver",
-      "yamlls",
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    cmd = { "MasonToolsInstall", "MasonToolsUpdate", "MasonToolsClean" },
+    dependencies = { "williamboman/mason.nvim" },
+    init = function()
+      require("astrocore").on_load("mason.nvim", function() require "mason-tool-installer" end)
+    end,
+    opts = {
+      ensure_installed = {
+        -- Language Servers
+        "clangd",
+        "cssls",
+        "gopls",
+        "hls",
+        "html",
+        "marksman",
+        "neocmake",
+        "jsonls",
+        "julials",
+        "lua_ls",
+        "taplo",
+        "texlab",
+        "yamlls",
 
-      -- Linters
-      "ansible-lint",
-      "shellcheck",
+        -- Linters
+        "shellcheck",
 
-      -- Formatters
-      "black",
-      "isort",
-      "prettierd",
-      "shfmt",
-      "stylua",
+        -- Formatters
+        "prettierd",
+        "shfmt",
+        "stylua",
 
-      -- Debuggers
-      "bash-debug-adapter",
-      "cpptools",
-      "debugpy",
-      "delve",
-      "js-debug-adapter",
-      "php-debug-adapter",
+        -- Debuggers
+        "bash-debug-adapter",
+        "cpptools",
+        "debugpy",
+        "delve",
+        "js-debug-adapter",
+        "php-debug-adapter",
+      },
+    },
+    config = function(_, opts)
+      local mason_tool_installer = require "mason-tool-installer"
+      mason_tool_installer.setup(opts)
+      mason_tool_installer.run_on_start()
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        -- "python",
+        "codelldb",
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      PATH = "append",
+      registries = {
+        "lua:custom-registry",
+        "github:mason-org/mason-registry",
+      },
     },
   },
-  config = function(_, opts)
-    local mason_tool_installer = require "mason-tool-installer"
-    mason_tool_installer.setup(opts)
-    mason_tool_installer.run_on_start()
-  end,
+  { "jay-babu/mason-nvim-dap.nvim", optional = true, init = false },
+  { "williamboman/mason-lspconfig.nvim", optional = true, init = false },
+  { "jay-babu/mason-null-ls.nvim", optional = true, init = false },
 }
