6bbee67d26581af196bb2e5dea960bcabf3434ac
Uzair Aftab <uzaaft@outlook.com>
2023-12-17 13:44:12 +0100

    feat: Disable cmp-line for now

--- lua/plugins/cmp-line.lua
+++ lua/plugins/cmp-line.lua
@@ -1,33 +1,35 @@
-return { -- override nvim-cmp plugin
-  "hrsh7th/nvim-cmp",
-  keys = { ":", "/", "?" }, -- lazy load cmp on more keys along with insert mode
-  dependencies = {
-    "hrsh7th/cmp-cmdline", -- add cmp-cmdline as dependency of cmp
-  },
-  config = function(_, opts)
-    local cmp = require "cmp"
-    -- run cmp setup
-    cmp.setup(opts)
+if true then return {} end
 
-    -- configure `cmp-cmdline` as described in their repo: https://github.com/hrsh7th/cmp-cmdline#setup
-    cmp.setup.cmdline("/", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        {
-          name = "cmdline",
-          option = {
-            ignore_cmds = { "Man", "!" },
-          },
-        },
-      }),
-    })
-  end,
-}
+-- return { -- override nvim-cmp plugin
+--   "hrsh7th/nvim-cmp",
+--   keys = { ":", "/", "?" }, -- lazy load cmp on more keys along with insert mode
+--   dependencies = {
+--     "hrsh7th/cmp-cmdline", -- add cmp-cmdline as dependency of cmp
+--   },
+--   config = function(_, opts)
+--     local cmp = require "cmp"
+--     -- run cmp setup
+--     cmp.setup(opts)
+--
+--     -- configure `cmp-cmdline` as described in their repo: https://github.com/hrsh7th/cmp-cmdline#setup
+--     cmp.setup.cmdline("/", {
+--       mapping = cmp.mapping.preset.cmdline(),
+--       sources = {
+--         { name = "buffer" },
+--       },
+--     })
+--     cmp.setup.cmdline(":", {
+--       mapping = cmp.mapping.preset.cmdline(),
+--       sources = cmp.config.sources({
+--         { name = "path" },
+--       }, {
+--         {
+--           name = "cmdline",
+--           option = {
+--             ignore_cmds = { "Man", "!" },
+--           },
+--         },
+--       }),
+--     })
+--   end,
+-- }
