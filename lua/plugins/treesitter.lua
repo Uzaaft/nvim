22c81bdb6526c347f793c3c8e23188a84e9b42cb
Micah Halter <micah@mehalter.com>
2023-12-18 09:04:50 -0500

    chore(treesitter): add TSConfig type

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = { "andymass/vim-matchup", init = function() vim.g.matchup_matchparen_deferred = 1 end },
+  ---@type TSConfig
+  ---@diagnostic disable-next-line: missing-fields
   opts = {
     auto_install = vim.fn.executable "tree-sitter" == 1,
     ensure_installed = { "bash", "markdown", "markdown_inline", "regex", "vim" },
