62044de79d08ad70bf1db0848531bfadec8b8d86
Uzair Aftab <uzaaft@outlook.com>
2023-12-21 09:28:09 +0100

    fix: move pylance stuff to lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,11 +4,11 @@ return {
     "AstroNvim/astrolsp",
     ---@type AstroLSPOpts
     opts = {
-      servers = { "vtsls" },
       features = {
         inlay_hints = true,
       },
       diagnostics = {
+        update_in_insert = false,
         virtual_text = true,
         underline = true,
       },
@@ -22,6 +22,7 @@ return {
         },
         timeout_ms = 1000, -- default format timeout
       },
+      ---@diagnostic disable: missing-fields
       config = {
         clangd = { capabilities = { offsetEncoding = "utf-8" } },
         julials = { autostart = false },
@@ -51,15 +52,120 @@ return {
             },
           },
         },
-      },
-      -- mappings to be set up on attaching of a language server
-      mappings = {
-        n = {
-          gl = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" },
+        pylance = {
+          filetypes = { "python" },
+          root_dir = function(...)
+            return require("lspconfig.util").root_pattern(unpack {
+              "pyproject.toml",
+              "setup.py",
+              "setup.cfg",
+              "requirements.txt",
+              "Pipfile",
+              "pyrightconfig.json",
+            })(...)
+          end,
+          cmd = { "pylance", "--stdio" },
+          single_file_support = true,
+          settings = {
+            python = {
+              pythonPath = vim.env.VIRTUAL_ENV and vim.env.VIRTUAL_ENV .. "/bin/python",
+              analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = "openFilesOnly",
+                typeCheckingMode = "basic",
+                autoImportCompletions = true,
+                completeFunctionParens = true,
+                indexing = true,
+                inlayHints = {
+                  variableTypes = true,
+                  functionReturnTypes = true,
+                  callArgumentNames = true,
+                  pytestParameters = true,
+                },
+                stubPath = vim.env.HOME .. "/typings",
+                diagnosticSeverityOverrides = {
+                  reportUnusedImport = "information",
+                  reportUnusedFunction = "information",
+                  reportUnusedVariable = "information",
+                  reportGeneralTypeIssues = "none",
+                  reportOptionalMemberAccess = "none",
+                  reportOptionalSubscript = "none",
+                  reportPrivateImportUsage = "none",
+                },
+              },
+            },
+          },
+          handlers = {
+            ["workspace/executeCommand"] = function(_, result)
+              if result and result.label == "Extract Method" then
+                vim.ui.input({ prompt = "New name: ", default = result.data.newSymbolName }, function(input)
+                  if input and #input > 0 then vim.lsp.buf.rename(input) end
+                end)
+              end
+            end,
+          },
+          commands = {
+            PylanceExtractMethod = {
+              function()
+                local arguments =
+                  { vim.uri_from_bufnr(0):gsub("file://", ""), require("vim.lsp.util").make_given_range_params().range }
+                vim.lsp.buf.execute_command { command = "pylance.extractMethod", arguments = arguments }
+              end,
+              description = "Extract Method",
+              range = 2,
+            },
+            PylanceExtractVariable = {
+              function()
+                local arguments =
+                  { vim.uri_from_bufnr(0):gsub("file://", ""), require("vim.lsp.util").make_given_range_params().range }
+                vim.lsp.buf.execute_command { command = "pylance.extractVariable", arguments = arguments }
+              end,
+              description = "Extract Variable",
+              range = 2,
+            },
+          },
+          docs = {
+            description = "https://github.com/microsoft/pylance-release\n\n`pylance`, Fast, feature-rich language support for Python",
+          },
         },
+        vtsls = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                parameterNames = { enabled = "all", suppressWhenArgumentMatchesName = false },
+                parameterTypes = { enabled = true },
+                variableTypes = { enabled = true, suppressWhenTypeMatchesName = false },
+                propertyDeclarationTypes = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                enumMemberValues = { enabled = true },
+              },
+              updateImportsOnFileMove = "always",
+            },
+            javascript = {
+              inlayHints = {
+                parameterNames = { enabled = "all", suppressWhenArgumentMatchesName = false },
+                parameterTypes = { enabled = true },
+                variableTypes = { enabled = true, suppressWhenTypeMatchesName = false },
+                propertyDeclarationTypes = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                enumMemberValues = { enabled = true },
+              },
+              updateImportsOnFileMove = "always",
+            },
+          },
+        },
+      },
+      capabilities = { workspace = { didChangeWatchedFiles = { dynamicRegistration = false } } },
+    },
+    -- mappings to be set up on attaching of a language server
+    mappings = {
+      n = {
+        gl = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" },
       },
     },
   },
+
   {
     "p00f/clangd_extensions.nvim",
     init = function()
