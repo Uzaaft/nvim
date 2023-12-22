b9d13b6af65fa7c6ec271063355b4625af93b52e
Micah Halter <micah@mehalter.com>
2023-12-22 14:56:04 -0500

    chore(lsp-file-operations): use experimental branch with more file operation support

--- lua/plugins/lsp-file-operations.lua
+++ lua/plugins/lsp-file-operations.lua
@@ -1,6 +1,7 @@
 return {
   -- "antosha417/nvim-lsp-file-operations",
   "mehalter/nvim-lsp-file-operations",
+  branch = "workspace_file_operations",
   lazy = true,
   init = function(plugin) require("astrocore").on_load("neo-tree.nvim", plugin.name) end,
   main = "lsp-file-operations",
