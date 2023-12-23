5be10548f3ced537b800c425ee63cb055f77c5bb
Uzair Aftab <uzaaft@outlook.com>
2023-12-23 23:15:06 +0100

    fix: polish noice config

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,25 +1,58 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  dependencies = { "rcarriga/nvim-notify", init = false, config = true },
-  opts = {
-    presets = {
-      inc_rename = true,
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
     },
-    cmdline = { view = "cmdline" },
-    messages = { view_search = false },
-    lsp = {
-      progress = { enabled = false },
-      hover = { enabled = false },
-      signature = { enabled = false },
-    },
-    routes = {
-      { filter = { event = "msg_show", cmdline = "^:lua" }, view = "messages" }, -- send lua output to split
-      { filter = { event = "msg_show", min_height = 20 }, view = "messages" }, -- send long messages to split
-      { filter = { event = "msg_show", find = "%d+L,%s%d+B" }, opts = { skip = true } }, -- skip save notifications
-      { filter = { event = "msg_show", find = "^%d+ more lines$" }, opts = { skip = true } }, -- skip paste notifications
-      { filter = { event = "msg_show", find = "^%d+ fewer lines$" }, opts = { skip = true } }, -- skip delete notifications
-      { filter = { event = "msg_show", find = "^%d+ lines yanked$" }, opts = { skip = true } }, -- skip yank notifications
+    init = function() vim.g.lsp_handlers_enabled = false end,
+    event = "VeryLazy",
+    cond = not (vim.g.neovide or vim.g.vscode),
+    opts = {
+      cmdline = { view = "cmdline" },
+      lsp = {
+        progress = { enabled = false },
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = { event = "msg_show", find = "%d+L,%s%d+B" },
+          opts = { skip = true },
+        }, -- skip save notifications
+        {
+          filter = { event = "msg_show", find = "^%d+ more line[s]?" },
+          opts = { skip = true },
+        }, -- skip paste notifications
+        {
+          filter = { event = "msg_show", find = "^%d+ fewer line[s]?" },
+          opts = { skip = true },
+        }, -- skip delete notifications
+        {
+          filter = { event = "msg_show", find = "^%d+ line[s]? less" },
+          opts = { skip = true },
+        }, -- skip delete notifications
+        {
+          filter = { event = "msg_show", find = "^%d+ change[s]?;" },
+          opts = { skip = true },
+        }, -- skip delete notifications
+        {
+          filter = { event = "msg_show", find = "^%d+ line[s]? yanked$" },
+          opts = { skip = true },
+        }, -- skip yank notifications
+        {
+          filter = { event = "msg_show", kind = "search_count" },
+          opts = { skip = true },
+        },
+        {
+          filter = { event = "msg_show", find = "Hop 1 char:" },
+          opts = { skip = true },
+        }, -- skip hop notifications
+      },
+      presets = { lsp_doc_border = true, long_message_to_split = true },
+      popupmenu = { backend = "cmp" },
     },
   },
 }
