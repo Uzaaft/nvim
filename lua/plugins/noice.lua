b79fd9ed58cb56a70718a88fc729f9f45fdae568
Uzair Aftab <uzaaft@outlook.com>
2023-12-28 16:15:04 +0100

    feat(noice): Go back to my toxic ex

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,58 +1,124 @@
+local opts = {
+  format = {
+    spinner = {
+      name = "mine",
+      hl = "Constant",
+    },
+  },
+  lsp = {
+    progress = {
+      enabled = false,
+      format = {
+        { "{data.progress.percentage} ", hl_group = "Comment" },
+        { "{spinner} ", hl_group = "NoiceLspProgressSpinner" },
+        { "{data.progress.title} ", hl_group = "Comment" },
+      },
+      format_done = {},
+    },
+    hover = { enabled = false },
+    signature = { enabled = false, auto_open = { enabled = false } },
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = false,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
+  cmdline = {
+    format = {
+      filter = { pattern = "^:%s*!", icon = " ", ft = "sh" },
+      IncRename = {
+        pattern = "^:%s*IncRename%s+",
+        icon = " ",
+        conceal = true,
+        opts = {
+          -- relative = "cursor",
+          -- size = { min_width = 20 },
+          -- position = { row = -3, col = 0 },
+          buf_options = { filetype = "text" },
+        },
+      },
+    },
+  },
+  popupmenu = {
+    enabled = true,
+  },
+  routes = {
+    {
+      view = "notify",
+      filter = { event = "msg_showmode" },
+    },
+    {
+      filter = {
+        event = "msg_show",
+        find = "%d+L, %d+B",
+      },
+      view = "mini",
+    },
+    {
+      view = "cmdline_output",
+      filter = { cmdline = "^:", min_height = 5 },
+      -- BUG: will be fixed after https://github.com/neovim/neovim/issues/21044 gets merged
+    },
+    {
+      filter = { event = "msg_show", kind = "search_count" },
+      opts = { skip = true },
+    },
+    {
+      filter = {
+        event = "msg_show",
+        find = "; before #",
+      },
+      opts = { skip = true },
+    },
+    {
+      filter = {
+        event = "msg_show",
+        find = "; after #",
+      },
+      opts = { skip = true },
+    },
+    {
+      filter = {
+        event = "msg_show",
+        find = " lines, ",
+      },
+      opts = { skip = true },
+    },
+    {
+      filter = {
+        event = "msg_show",
+        find = "go up one level",
+      },
+      opts = { skip = true },
+    },
+    {
+      filter = {
+        event = "msg_show",
+        find = "yanked",
+      },
+      opts = { skip = true },
+    },
+    {
+      filter = { find = "No active Snippet" },
+      opts = { skip = true },
+    },
+    {
+      filter = { find = "waiting for cargo metadata" },
+      opts = { skip = true },
+    },
+  },
+}
+
 return {
-  {
-    "folke/noice.nvim",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-    },
-    init = function() vim.g.lsp_handlers_enabled = false end,
-    event = "VeryLazy",
-    cond = not (vim.g.neovide or vim.g.vscode),
-    opts = {
-      cmdline = { view = "cmdline" },
-      lsp = {
-        progress = { enabled = false },
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      routes = {
-        {
-          filter = { event = "msg_show", find = "%d+L,%s%d+B" },
-          opts = { skip = true },
-        }, -- skip save notifications
-        {
-          filter = { event = "msg_show", find = "^%d+ more line[s]?" },
-          opts = { skip = true },
-        }, -- skip paste notifications
-        {
-          filter = { event = "msg_show", find = "^%d+ fewer line[s]?" },
-          opts = { skip = true },
-        }, -- skip delete notifications
-        {
-          filter = { event = "msg_show", find = "^%d+ line[s]? less" },
-          opts = { skip = true },
-        }, -- skip delete notifications
-        {
-          filter = { event = "msg_show", find = "^%d+ change[s]?;" },
-          opts = { skip = true },
-        }, -- skip delete notifications
-        {
-          filter = { event = "msg_show", find = "^%d+ line[s]? yanked$" },
-          opts = { skip = true },
-        }, -- skip yank notifications
-        {
-          filter = { event = "msg_show", kind = "search_count" },
-          opts = { skip = true },
-        },
-        {
-          filter = { event = "msg_show", find = "Hop 1 char:" },
-          opts = { skip = true },
-        }, -- skip hop notifications
-      },
-      presets = { lsp_doc_border = true, long_message_to_split = true },
-      popupmenu = { backend = "cmp" },
-    },
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = opts,
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
   },
 }
