0a2cb1edf33443fa51ff9475d0abf454c7a9f8e5
Uzair Aftab <uzaaft@outlook.com>
2023-12-28 21:29:51 +0100

    chore: steal mehalters shit

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,45 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      results_title = "",
+      selection_caret = "  ",
+      layout_config = {
+        width = 0.90,
+        height = 0.85,
+        preview_cutoff = 120,
+        horizontal = {
+          preview_width = 0.6,
+        },
+        vertical = {
+          width = 0.9,
+          height = 0.95,
+          preview_height = 0.5,
+        },
+        flex = {
+          horizontal = {
+            preview_width = 0.9,
+          },
+        },
+      },
+    },
+    pickers = {
+      find_files = {
+        current_buffer_fuzzy_find = { sorting_strategy = "ascending" },
+        hidden = true,
+        find_command = function(cfg)
+          local find_command = { "rg", "--files", "--color", "never" }
+          if not cfg.no_ignore then vim.list_extend(find_command, { "--glob", "!**/.git/**" }) end
+          return find_command
+        end,
+      },
+      buffers = {
+        path_display = { "smart" },
+        mappings = {
+          i = { ["<C-d>"] = function(...) return require("telescope.actions").delete_buffer(...) end },
+          n = { ["d"] = function(...) return require("telescope.actions").delete_buffer(...) end },
+        },
+      },
+    },
+  },
+}
