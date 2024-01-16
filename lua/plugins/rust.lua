568c4a255874a6046bc72e23555a5901d7168b3e
Uzair Aftab <uzaaft@outlook.com>
2024-01-16 10:17:06 +0100

    feat: move to rustaceanvim

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,24 +1,33 @@
 return {
   {
-    "vxpm/ferris.nvim",
-    lazy = true,
-    init = function()
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("lazy_ferris", { clear = true }),
-        desc = "Lazy load Ferris",
-        once = true,
-        callback = function(args)
-          if vim.lsp.get_client_by_id(args.data.client_id).name == "rust_analyzer" then
-            if require("ferris.private.config").opts.create_commands then
-              require("ferris").create_commands(args.buf)
-            end
-          end
-        end,
-      })
+    "mrcjkb/rustaceanvim",
+    version = "^3",
+    ft = "rust",
+    opts = function()
+      local adapter
+      local success, package = pcall(function() return require("mason-registry").get_package "codelldb" end)
+      local cfg = require "rustaceanvim.config"
+      if success then
+        local package_path = package:get_install_path()
+        local codelldb_path = package_path .. "/codelldb"
+        local liblldb_path = package_path .. "/extension/lldb/lib/liblldb"
+        local this_os = vim.loop.os_uname().sysname
+
+        -- The path in windows is different
+        if this_os:find "Windows" then
+          codelldb_path = package_path .. "\\extension\\adapter\\codelldb.exe"
+          liblldb_path = package_path .. "\\extension\\lldb\\bin\\liblldb.dll"
+        else
+          -- The liblldb extension is .so for linux and .dylib for macOS
+          liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+        end
+        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
+      else
+        adapter = cfg.get_codelldb_adapter()
+      end
+      return { server = require("astrolsp").lsp_opts "rust_analyzer", dap = { adapter = adapter } }
     end,
-    opts = {
-      url_handler = function(str) require("astrocore").system_open(str) end,
-    },
+    config = function(_, opts) vim.g.rustaceanvim = opts end,
   },
   {
     "Saecki/crates.nvim",
