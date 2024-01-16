f73bf6444feb5b2ff419341770f731d42a756ae9
Uzair Aftab <uzaaft@outlook.com>
2024-01-16 10:22:09 +0100

    feat: Make conform.lua more dynamic

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -67,11 +67,41 @@ return {
         if autoformat == nil then autoformat = vim.g.autoformat end
         if autoformat then return { timeout_ms = 500, lsp_fallback = true } end
       end
+      -- Check to see if pretties config is in root
+      local prettier_config_files = {
+        ".prettierrc",
+        ".prettierrc.json",
+        ".prettierrc.yml",
+        ".prettierrc.yaml",
+        ".prettierrc.json5",
+        ".prettierrc.js",
+        ".prettierrc.cjs",
+        "prettier.config.js",
+        ".prettierrc.mjs",
+        "prettier.config.mjs",
+        "prettier.config.cjs",
+        ".prettierrc.toml",
+      }
+      local prettier_config = nil
+      for _, file in ipairs(prettier_config_files) do
+        if vim.fn.filereadable(file) == 1 then
+          prettier_config = file
+          break
+        end
+      end
+      -- Check to see if biome.json is in root
+      local biome_config = nil
+      if vim.fn.filereadable "biome.json" == 1 then biome_config = "biome.json" end
+      -- If biome, use biome, else use prettier
+      local formatter = biome_config ~= nil and "biome" or "prettierd"
 
       opts.formatters_by_ft = {
         ["*"] = { "injected" },
         lua = { "stylua" },
-        typescriptreact = { "prettierd" },
+        typescriptreact = { formatter },
+        typescript = { formatter },
+        javascriptreact = { formatter },
+        javascript = { formatter },
         python = function(bufnr)
           return require("conform").get_formatter_info("ruff_format", bufnr).available and { "ruff_format" }
             or { "isort", "black" }
