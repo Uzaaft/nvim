5d3880500e03632381e596a06ccae13fee986c1d
Micah Halter <micah@mehalter.com>
2024-01-19 13:32:53 -0500

    fix(mason): update to new `list_insert_unique` syntax

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,9 +1,7 @@
 return {
   "williamboman/mason.nvim",
   opts = function(_, opts)
-    opts.registries = require("astrocore").list_insert_unique(
-      opts.registries,
-      { "lua:custom-registry", "github:mason-org/mason-registry" }
-    )
+    opts.registries =
+      require("astrocore").list_insert_unique(opts.registries, "lua:custom-registry", "github:mason-org/mason-registry")
   end,
 }
