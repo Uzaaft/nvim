a80a6c2064ab478306738d539c71e1087b8257a4
Micah Halter <micah@mehalter.com>
2024-01-23 11:28:14 -0500

    chore(lsp-file-operations): switch back to upstream repo

--- lua/plugins/lsp-file-operations.lua
+++ lua/plugins/lsp-file-operations.lua
@@ -1,8 +1,6 @@
 ---@type LazySpec
 return {
-  -- "antosha417/nvim-lsp-file-operations",
-  "mehalter/nvim-lsp-file-operations",
-  branch = "workspace_file_operations",
+  "antosha417/nvim-lsp-file-operations",
   lazy = true,
   init = function(plugin) require("astrocore").on_load("neo-tree.nvim", plugin.name) end,
   main = "lsp-file-operations",
