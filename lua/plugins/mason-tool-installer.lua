8beeb06027dd217ffb0f24b84073021ef79e8c44
Uzair Aftab <uzaaft@outlook.com>
2024-01-28 12:15:37 +0100

    chore: update syntax

--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -56,10 +56,7 @@ return {
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        -- "python",
-        "codelldb",
-      })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "codelldb")
     end,
   },
   {
