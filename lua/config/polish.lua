f6bb80a2d3dd58f02c42830613dc47738236d416
Uzair Aftab <uzaaft@outlook.com>
2024-01-28 11:47:29 +0100

    feat: Add justfile filename detection

--- lua/config/polish.lua
+++ lua/config/polish.lua
@@ -2,12 +2,17 @@ vim.filetype.add {
   extension = {
     mdx = "markdown.mdx",
     qmd = "markdown",
+    just = "just",
   },
   filename = {
     [".eslintrc.json"] = "jsonc",
     [".env"] = "sh",
     [".env.local"] = "sh",
     ["docker-compose.yaml"] = "yaml.docker-compose",
+    ["justfile"] = "just",
+    ["Justfile"] = "just",
+    [".Justfile"] = "just",
+    [".justfile"] = "just",
   },
   pattern = {
     ["tsconfig*.json"] = "jsonc",
