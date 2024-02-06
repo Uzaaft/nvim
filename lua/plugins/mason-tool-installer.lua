e7a0248bda08f75ccc0de9b11d6b7192843ec53c
Micah Halter <micah@mehalter.com>
2024-02-06 10:34:58 -0500

    chore(mason-tool-installer): update deps

--- lua/plugins/mason-tool-installer.lua
+++ lua/plugins/mason-tool-installer.lua
@@ -21,7 +21,7 @@ return {
         "lua_ls",
         "marksman",
         "neocmake",
-        { "pylance", version = "2023.12.104" }, -- last known working version
+        { "pylance", version = "2024.1.104" }, -- last known working version
         "tailwindcss",
         "taplo",
         "texlab",
