2674114b234ed5bec24c7ad664b904bf97f3100c
Micah Halter <micah@mehalter.com>
2024-02-09 10:49:18 -0500

    refactor(cmp): simplify `nvim-cmp` implementation

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,44 +8,39 @@ return {
     "kdheepak/cmp-latex-symbols",
   },
   opts = function(_, opts)
-    local cmp = require "cmp"
-    local compare = require "cmp.config.compare"
+    opts.sources = {
+      { name = "nvim_lsp", priority = 1000 },
+      { name = "luasnip", priority = 750 },
+      { name = "pandoc_references", priority = 725 },
+      { name = "latex_symbols", priority = 700 },
+      { name = "emoji", priority = 700 },
+      { name = "calc", priority = 650 },
+      { name = "path", priority = 500 },
+      { name = "buffer", priority = 250 },
+    }
 
-    return require("astrocore").extend_tbl(opts, {
-      window = { completion = { col_offset = -1, side_padding = 0 } },
-      sources = cmp.config.sources {
-        { name = "nvim_lsp", priority = 1000 },
-        { name = "luasnip", priority = 750 },
-        { name = "pandoc_references", priority = 725 },
-        { name = "latex_symbols", priority = 700 },
-        { name = "emoji", priority = 700 },
-        { name = "calc", priority = 650 },
-        { name = "path", priority = 500 },
-        { name = "buffer", priority = 250 },
-      },
-      sorting = {
-        comparators = {
-          compare.offset,
-          compare.exact,
-          compare.score,
-          compare.recently_used,
-          function(entry1, entry2)
-            local _, entry1_under = entry1.completion_item.label:find "^_+"
-            local _, entry2_under = entry2.completion_item.label:find "^_+"
-            entry1_under = entry1_under or 0
-            entry2_under = entry2_under or 0
-            if entry1_under > entry2_under then
-              return false
-            elseif entry1_under < entry2_under then
-              return true
-            end
-          end,
-          compare.kind,
-          compare.sort_text,
-          compare.length,
-          compare.order,
-        },
-      },
-    })
+    if not opts.sorting then opts.sorting = {} end
+    local compare = require "cmp.config.compare"
+    opts.sorting.comparators = {
+      compare.offset,
+      compare.exact,
+      compare.score,
+      compare.recently_used,
+      function(entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find "^_+"
+        local _, entry2_under = entry2.completion_item.label:find "^_+"
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
+      compare.kind,
+      compare.sort_text,
+      compare.length,
+      compare.order,
+    }
   end,
 }
