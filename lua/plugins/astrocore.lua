22bbacffea0503d08282e59fa67d7b9142b21355
Micah Halter <micah@mehalter.com>
2024-02-13 10:38:42 -0500

    refactor(options): options moved to AstroCore

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -4,6 +4,20 @@ local opts = {
     ignore = { servers = { "julials" } },
     autochdir = true,
   },
+  options = {
+    opt = {
+      conceallevel = 2, -- enable conceal
+      list = true, -- show whitespace characters
+      listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
+      showbreak = "↪ ",
+      showtabline = (vim.t.bufs and #vim.t.bufs > 1) and 2 or 1,
+      spellfile = vim.fn.expand "~/.config/nvim/spell/en.utf-8.add",
+      splitkeep = "screen",
+      swapfile = false,
+      thesaurus = vim.fn.expand "~/.config/nvim/spell/mthesaur.txt",
+      wrap = true, -- soft wrap lines
+    },
+  },
   autocmds = {
     auto_spell = {
       {
