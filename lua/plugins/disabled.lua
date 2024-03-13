1efd9e015cbec0c95b9276ccaa815b51a8ba4125
Uzair Aftab <muaf@nmbu.no>
2024-03-13 22:13:37 +0100

    feat: move disabled plugins to seperate file

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+return vim.tbl_map(function(plugin) return { plugin, enabled = false } end, {
+  "lukas-reineke/indent-blankline.nvim",
+  "goolord/alpha-nvim",
+  "max397574/better-escape.nvim",
+  "nvimtools/none-ls.nvim",
+  "jay-babu/mason-null-ls.nvim",
+  "L3MON4D3/LuaSnip",
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,14 +1,9 @@
 ---@type LazySpec[]
 return {
   -- disable core plugins
-  { "max397574/better-escape.nvim", enabled = false },
-  { "nvimtools/none-ls.nvim", enabled = false },
-  { "jay-babu/mason-null-ls.nvim", enabled = false },
   { "folke/which-key.nvim", enabled = false },
-  { "alpha-nvim", enabled = false }, -- disable starter
   -- Very small specs not worth their own files
   { "akinsho/toggleterm.nvim", opts = { terminal_mappings = false } },
-  { "L3MON4D3/LuaSnip", enabled = false },
   {
     "rcarriga/nvim-notify",
     opts = {
@@ -49,18 +44,4 @@ return {
     version = false,
   },
   { "justinsgithub/wezterm-types" },
-  {
-    "NTBBloodbath/zig-tools.nvim",
-    -- Load zig-tools.nvim only in Zig buffers
-    ft = { "zig" },
-    opts = {},
-    dependencies = {
-      {
-        "akinsho/toggleterm.nvim",
-      },
-      {
-        "nvim-lua/plenary.nvim",
-      },
-    },
-  },
-  }
+}
