0c909a77493b4483ff64d0dec61654ae31306da2
Micah Halter <micah@mehalter.com>
2024-04-01 09:28:48 -0400

    chore: move to released AstroNvim

--- lua/base.lua
+++ lua/base.lua
@@ -1,10 +1,6 @@
 ---@LazySpec
 return {
-  {
-    "AstroNvim/AstroNvim",
-    branch = "v4", -- version = "^4",
-    import = "astronvim.plugins",
-  },
+  { "AstroNvim/AstroNvim", import = "astronvim.plugins" },
   -- { "AstroNvim/astrocommunity" },
   -- { import = "astrocommunity.pack.lua" },
 }
