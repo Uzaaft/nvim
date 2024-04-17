ee2dea2734f55ca1c1f23a297ccd9c02177de1fe
Micah Halter <micah@mehalter.com>
2024-04-17 10:31:35 -0400

    chore(lsp-file-operations): remove unnecessary `lazy = true`

--- lua/plugins/lsp-file-operations.lua
+++ lua/plugins/lsp-file-operations.lua
@@ -1,7 +1,6 @@
 ---@type LazySpec
 return {
   "antosha417/nvim-lsp-file-operations",
-  lazy = true,
   init = function(plugin) require("astrocore").on_load("neo-tree.nvim", plugin.name) end,
   main = "lsp-file-operations",
   opts = {},
