eefde1b4f834bb60223ba4211ecd5a8c2fa2894d
Micah Halter <micah@mehalter.com>
2024-05-03 09:54:32 -0400

    feat: disable inlay hints on insert if enabled

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -3,6 +3,26 @@ return {
   "AstroNvim/astrolsp",
   ---@type AstroLSPOpts
   opts = {
+    autocmds = {
+      no_insert_inlay_hints = {
+        cond = vim.lsp.inlay_hint and "textDocument/inlayHint" or false,
+        {
+          event = "InsertEnter",
+          desc = "disable inlay hints on insert",
+          callback = function(args)
+            local filter = { bufnr = args.buf }
+            if vim.lsp.inlay_hint.is_enabled(filter) then
+              vim.lsp.inlay_hint.enable(false, filter)
+              vim.api.nvim_create_autocmd("InsertLeave", {
+                buffer = args.buf,
+                once = true,
+                callback = function() vim.lsp.inlay_hint.enable(true, filter) end,
+              })
+            end
+          end,
+        },
+      },
+    },
     ---@diagnostic disable: missing-fields
     config = {
       basedpyright = {
