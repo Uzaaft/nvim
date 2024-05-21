b7f0351b0ad0afa30fa7f2a29d4f0800b2b6b7a9
Micah Halter <micah@mehalter.com>
2024-05-21 09:29:55 -0400

    feat(plugins): move to native neovim commenting rather than `Comment.nvim`

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,8 +1,10 @@
----@type LazySpec
-return vim.tbl_map(function(plugin) return { plugin, enabled = false } end, {
-  "lukas-reineke/indent-blankline.nvim",
+local disabled = {
   "goolord/alpha-nvim",
+  "jay-babu/mason-null-ls.nvim",
+  "lukas-reineke/indent-blankline.nvim",
   "max397574/better-escape.nvim",
   "nvimtools/none-ls.nvim",
-  "jay-babu/mason-null-ls.nvim",
-})
+}
+if vim.fn.has "nvim-0.10" == 1 then table.insert(disabled, "numToStr/Comment.nvim") end
+
+return vim.tbl_map(function(plugin) return { plugin, enabled = false } end, disabled)
