a8c7a089bcf922edc02ca0065a3d957909cf2367
Uzair Aftab <muaf@nmbu.no>
2024-06-16 19:28:18 +0200

    feat: disable LuaSnip and codeium

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,6 +4,9 @@ local disabled = {
   "lukas-reineke/indent-blankline.nvim",
   "max397574/better-escape.nvim",
   "nvimtools/none-ls.nvim",
+  -- Custom disable stuff
+  "L3MON4D3/LuaSnip",
+  "Exafunction/codeium.vim",
 }
 if vim.fn.has "nvim-0.10" == 1 then table.insert(disabled, "numToStr/Comment.nvim") end
 
