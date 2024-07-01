8d652f4dbeefa48f8a7af06bf11c82937693cfa8
Uzair Aftab <muaf@nmbu.no>
2024-07-01 11:29:19 +0200

    chore(disabled): Add rainbow-delimiters

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -6,6 +6,7 @@ local disabled = {
   "nvimtools/none-ls.nvim",
   -- Custom disable stuff
   "Exafunction/codeium.vim",
+  "HiPhish/rainbow-delimiters.nvim",
 }
 if vim.fn.has "nvim-0.10" == 1 then table.insert(disabled, "numToStr/Comment.nvim") end
 
