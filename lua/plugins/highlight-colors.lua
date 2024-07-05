1fca28aed8889e1d3e9b2cc75503fd199587a11c
Micah Halter <micah@mehalter.com>
2024-07-05 15:23:45 -0400

    refactor(highlight-colors): `cmp` integration moved upstream

--- lua/plugins/highlight-colors.lua
+++ lua/plugins/highlight-colors.lua
@@ -13,19 +13,4 @@ return {
     },
   },
   opts = { enable_named_colors = false, virtual_symbol = "" },
-}, {
-  "hrsh7th/nvim-cmp",
-  optional = true,
-  opts = function(_, opts)
-    if not opts.formatting then opts.formatting = {} end
-    local formatter = vim.tbl_get(opts, "formatting", "format")
-    opts.formatting.format = function(entry, item)
-      local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
-      if formatter then item = formatter(entry, item) end
-      if color_item.abbr_hl_group then
-        item.kind, item.kind_hl_group = color_item.abbr, color_item.abbr_hl_group
-      end
-      return item
-    end
-  end,
 }
