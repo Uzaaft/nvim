a9a0de54a89b01af6ab685b084810bb39e2d0347
Micah Halter <micah@mehalter.com>
2024-07-11 09:14:49 -0400

    feat(mini-icons): add some more file icons

--- lua/plugins/mini-icons.lua
+++ lua/plugins/mini-icons.lua
@@ -1,6 +1,23 @@
 return {
   "echasnovski/mini.icons",
-  opts = {},
+  opts = {
+    file = {
+      [".go-version"] = { glyph = "", hl = "MiniIconsBlue" },
+      [".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+      [".node-version"] = { glyph = "", hl = "MiniIconsGreen" },
+      [".prettierrc"] = { glyph = "", hl = "MiniIconsPurple" },
+      [".yarnrc.yml"] = { glyph = "", hl = "MiniIconsBlue" },
+      ["eslint.config.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+      ["package.json"] = { glyph = "", hl = "MiniIconsGreen" },
+      ["tsconfig.json"] = { glyph = "", hl = "MiniIconsAzure" },
+      ["tsconfig.build.json"] = { glyph = "", hl = "MiniIconsAzure" },
+      ["yarn.lock"] = { glyph = "", hl = "MiniIconsBlue" },
+    },
+    filetype = {
+      gotmpl = { glyph = "󰟓", hl = "MiniIconsGrey" },
+      ["hcl.packer"] = { glyph = "", hl = "MiniIconsAzure" },
+    },
+  },
   init = function()
     package.preload["nvim-web-devicons"] = function()
       require("mini.icons").mock_nvim_web_devicons()
