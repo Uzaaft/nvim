a86e7ef6239ebce4387e85180c70b74fdc4b43bf
Uzair Aftab <muaf@nmbu.no>
2024-07-20 14:04:39 +0200

    feat(astroui): Use catppuccin-machiato

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -3,7 +3,7 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    colorscheme = "catppuccin",
+    colorscheme = "catppuccin-macchiato",
     highlights = {
       init = function(colors_name)
         local get_hlgroup = require("astroui").get_hlgroup
