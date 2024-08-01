e8d4d9717ad0f5fc630eb270cfb2fd6efd76cba4
Uzair Aftab <muaf@nmbu.no>
2024-08-01 13:50:52 +0200

    feat(uzaaft): 🫃🏼

--- lua/plugins/uzaaft.lua
+++ lua/plugins/uzaaft.lua
@@ -1,22 +1,6 @@
 ---@type LazySpec
+
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    optional = true,
-    opts = function(_, opts)
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.zig = {
-        install_info = {
-          url = "https://github.com/maxxnino/tree-sitter-zig", -- local path or git repo
-          files = { "src/parser.c", "src/scanner.cc" },
-          branch = "main",
-          use_makefile = true, -- this may be necessary on MacOS (try if you see compiler errors)
-        },
-        maintainers = { "@maxxnino" },
-      }
-      require("astrocore").list_insert_unique(opts.ensure_installed, { "zig" })
-    end,
-  },
   {
     "https://codeberg.org/NTBBloodbath/zig-tools.nvim",
     -- Load zig-tools.nvim only in Zig buffers
@@ -25,6 +9,49 @@ return {
     dependencies = {
       "akinsho/toggleterm.nvim",
       "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "tpope/vim-dadbod",
+    specs = {
+      {
+        "kristijanhusak/vim-dadbod-ui",
+        dependencies = { "tpope/vim-dadbod" },
+        event = "VeryLazy",
+      },
+      {
+        "hrsh7th/nvim-cmp",
+        optional = true,
+        dependencies = {
+          {
+            "kristijanhusak/vim-dadbod-completion",
+            dependencies = {
+              "AstroNvim/astrocore",
+              opts = {
+                autocmds = {
+                  dadbod_cmp = {
+                    {
+                      event = "FileType",
+                      desc = "dadbod completion",
+                      pattern = { "sql", "mysql", "plsql" },
+                      callback = function()
+                        require("cmp").setup.buffer { sources = { { name = "vim-dadbod-completion" } } }
+                      end,
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "m4xshen/smartcolumn.nvim",
+    event = { "InsertEnter", "User AstroFile" },
+    opts = {
+      disabled_filetypes = { "alpha", "neo-tree", "ministarter", "help", "text", "markdown", "oil" },
     },
   },
 }
