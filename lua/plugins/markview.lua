9c7e6eb8b25e306357f4bf081b817341140f1211
Micah Halter <micah@mehalter.com>
2024-08-08 11:32:19 -0400

    feat(plugins): add `markview.nvim`

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,10 @@
+return {
+  "OXY2DEV/markview.nvim",
+  ft = { "markdown", "markdown.mdx" },
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    hybrid_modes = { "n" },
+    headings = { shift_width = 0 },
+    code_blocks = { sign = false },
+  },
+}
