52ff83ebdf39b117ffdfc68d3af13db90e0b7316
Micah Halter <micah@mehalter.com>
2024-08-15 13:07:19 -0400

    fix(trouble): improve trouble mappings

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -29,16 +29,16 @@ return {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local maps, prefix = opts.mappings, "<Leader>x"
-        maps.n[prefix .. "L"] = { "<Cmd>Trouble loclist toggle<CR>", desc = "Location List (Trouble)" }
-        maps.n[prefix .. "Q"] = { "<Cmd>Trouble qflist toggle<CR>", desc = "Quickfix List (Trouble)" }
-        maps.n[prefix .. "x"] = { "<Cmd>Trouble diagnostics toggle<CR>", desc = "Diagnostics (Trouble)" }
+        maps.n[prefix .. "L"] = { "<Cmd>Trouble loclist toggle focus=true<CR>", desc = "Location List (Trouble)" }
+        maps.n[prefix .. "Q"] = { "<Cmd>Trouble qflist toggle focus=true<CR>", desc = "Quickfix List (Trouble)" }
+        maps.n[prefix .. "x"] = { "<Cmd>Trouble diagnostics toggle focus=true<CR>", desc = "Diagnostics (Trouble)" }
         maps.n[prefix .. "X"] =
-          { "<Cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Buffer Diagnostics (Trouble)" }
+          { "<Cmd>Trouble diagnostics toggle focus=true filter.buf=0<CR>", desc = "Buffer Diagnostics (Trouble)" }
 
         if require("astrocore").is_available "todo-comments.nvim" then
-          maps.n[prefix .. "t"] = { "<Cmd>Trouble todo<CR>", desc = "Todo (Trouble)" }
-          maps.n[prefix .. "T"] =
-            { "<Cmd>Trouble todo filter={tag={TODO,FIX,FIXME}}<CR>", desc = "Todo/Fix/Fixme (Trouble)" }
+          maps.n[prefix .. "t"] =
+            { "<Cmd>Trouble todo toggle focus=true filter.buf=0<CR>", desc = "Todo buffer (Trouble)" }
+          maps.n[prefix .. "T"] = { "<Cmd>Trouble todo toggle focus=true<CR>", desc = "Todo (Trouble)" }
         end
       end,
     },
