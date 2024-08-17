7d6982292c51afd82f86acd51c745e55e9c8b0d0
Micah Halter <micah@mehalter.com>
2024-08-17 09:11:34 -0400

    fix(conform): limit updates when using nvim 0.9

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,6 +2,7 @@
 return {
   "stevearc/conform.nvim",
   event = "User AstroFile",
+  version = vim.fn.has "nvim-0.10" ~= 1 and "7",
   cmd = "ConformInfo",
   dependencies = { "williamboman/mason.nvim" },
   ---@param opts conform.setupOpts
