5d88209bb710db4b6b1fb0ecc346e87c2211669b
Micah Halter <micah@mehalter.com>
2024-08-19 16:45:13 -0400

    feat(plugins): use `better-escape.nvim` for binding to quit terminals

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,7 +2,6 @@ local disabled = {
   "goolord/alpha-nvim",
   "jay-babu/mason-null-ls.nvim",
   "lukas-reineke/indent-blankline.nvim",
-  "max397574/better-escape.nvim",
   "nvimtools/none-ls.nvim",
 }
 if vim.fn.has "nvim-0.10" == 1 then table.insert(disabled, "numToStr/Comment.nvim") end
