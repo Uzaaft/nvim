6783f19dd5fced0a0b573b25949410670fc46764
Micah Halter <micah@mehalter.com>
2024-08-19 13:54:28 -0400

    fix(markview): disable cursor conceal in normal mode

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -6,5 +6,11 @@ return {
     hybrid_modes = { "n" },
     headings = { shift_width = 0 },
     code_blocks = { sign = false },
+    callbacks = {
+      on_enable = function(_, win)
+        vim.wo[win].conceallevel = 2
+        vim.wo[win].concealcursor = "c"
+      end,
+    },
   },
 }
