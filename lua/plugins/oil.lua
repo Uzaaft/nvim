33d4422f10efbad157c9de7936e58330b7647c6b
Micah Halter <micah@mehalter.com>
2024-08-29 14:29:43 -0400

    feat(oil): add `gd` to toggle detailed view

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -42,14 +42,25 @@ return {
       orig_refresh(...)
     end
 
+    local columns = {
+      icon = { "icon", default_file = get_icon "DefaultFile", directory = get_icon "FolderClosed" },
+      permissions = { "permissions", highlight = "Type" },
+      size = { "size", highlight = "String" },
+      mtime = { "mtime", highlight = "Function" },
+    }
+    local simple, detailed = { columns.icon }, { columns.permissions, columns.size, columns.mtime, columns.icon }
+
     ---@type oil.setupOpts
     return {
-      columns = {
-        { "icon", default_file = get_icon "DefaultFile", directory = get_icon "FolderClosed" },
-      },
+      columns = simple,
       skip_confirm_for_simple_edits = true,
+      prompt_save_on_select_new_entry = false,
       watch_for_changes = true,
       keymaps = {
+        gd = {
+          desc = "Toggle detailed file view",
+          callback = function() require("oil").set_columns(#require("oil.config").columns == 1 and detailed or simple) end,
+        },
         R = "actions.refresh",
         ["<Tab>"] = "actions.close",
       },
