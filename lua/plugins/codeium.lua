6402c91066f02094b11e1bd4d7e139a7f15fe7f1
Micah Halter <micah@mehalter.com>
2024-09-09 16:07:02 -0400

    feat(codeium): use new user events

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -6,31 +6,21 @@ return {
       "AstroNvim/astrocore",
       opts = function(_, opts)
         local maps = opts.mappings
-        maps.n["<Leader>u!"] = {
-          function()
-            require("neocodeium.commands").toggle()
-            vim.cmd.redrawstatus()
-          end,
-          desc = "Toggle AI assistant",
-        }
+        maps.n["<Leader>u!"] = { function() require("neocodeium.commands").toggle() end, desc = "Toggle AI assistant" }
         maps.n["<M-CR>"] = { function() require("neocodeium").chat() end }
-        maps.i["<M-]>"] = {
-          function()
-            local cmp_avail, cmp = pcall(require, "cmp")
-            if cmp_avail then cmp.abort() end
-            require("neocodeium").cycle_or_complete()
-          end,
-        }
+        maps.i["<M-]>"] = { function() require("neocodeium").cycle_or_complete() end }
         maps.i["<M-\\>"] = maps.i["<M-]>"]
-        maps.i["<M-[>"] = {
-          function()
-            local cmp_avail, cmp = pcall(require, "cmp")
-            if cmp_avail then cmp.abort() end
-            require("neocodeium").cycle_or_complete(-1)
-          end,
-        }
+        maps.i["<M-[>"] = { function() require("neocodeium").cycle_or_complete(-1) end }
         maps.i["<M-CR>"] = { function() require("neocodeium").accept() end }
         maps.i["<M-BS>"] = { function() require("neocodeium").clear() end }
+
+        opts.autocmds.codeium = {
+          {
+            event = "User",
+            pattern = "NeoCodeiumCompletionDisplayed",
+            callback = function() require("cmp").abort() end,
+          },
+        }
       end,
     },
     {
@@ -46,16 +36,28 @@ return {
               provider = function()
                 local aistatus, serverstatus = require("neocodeium").get_status()
                 return status.utils.stylize(
-                  (aistatus ~= 0 and "󱚧") or (serverstatus == 0 and "󰚩" or "󱚢"),
+                  (aistatus ~= 0 and (aistatus == 1 and "󱚧" or "󱙻")) or (serverstatus == 0 and "󰚩" or "󱚢"),
                   { padding = { right = 1 } }
                 )
               end,
             },
             hl = { fg = "git_branch_fg" },
+            update = { "BufEnter", callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end) },
+            init = status.init.update_events {
+              {
+                "User",
+                pattern = { "NeoCodeiumServer*", "NeoCodeium*{En,Dis}abled" },
+                callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end),
+              },
+            },
             surround = {
               separator = "right",
               color = "section_bg",
-              condition = function() return vim.g.neocodeium_enabled end,
+              condition = function()
+                local enabled = vim.g.neocodeium_enabled
+                if enabled ~= false and vim.b.neocodeium_enabled ~= nil then enabled = vim.b.neocodeium_enabled end
+                return enabled
+              end,
             },
           }
         )
