e83c9fe8297a06c7844fe859eb0f5be1b1e1cfbd
Micah Halter <micah@mehalter.com>
2024-09-09 08:59:15 -0400

    feat(codeium): move from `codeium.vim` to `neocodeium`

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,21 +1,62 @@
 return {
-  "Exafunction/codeium.vim",
-  event = "BufEnter",
+  "monkoose/neocodeium",
+  cmd = "NeoCodeium",
   dependencies = {
     {
       "AstroNvim/astrocore",
       opts = function(_, opts)
-        local options, maps = opts.options, opts.mappings
-        options.g.codeium_disable_bindings = 1
-        options.g.codeium_manual = true
-
-        maps.n["<M-CR>"] = { function() return vim.fn["codeium#Chat"]() end, expr = true, silent = true }
-        maps.i["<M-]>"] = { function() return vim.fn["codeium#CycleOrComplete"]() end, expr = true, silent = true }
+        local maps = opts.mappings
+        maps.n["<Leader>u!"] = {
+          function()
+            require("neocodeium.commands").toggle()
+            vim.cmd.redrawstatus()
+          end,
+          desc = "Toggle AI assistant",
+        }
+        maps.n["<M-CR>"] = { function() require("neocodeium").chat() end }
+        maps.i["<M-]>"] = { function() require("neocodeium").cycle_or_complete() end }
         maps.i["<M-\\>"] = maps.i["<M-]>"]
-        maps.i["<M-[>"] = { function() return vim.fn["codeium#CycleCompletions"](-1) end, expr = true, silent = true }
-        maps.i["<M-CR>"] = { function() return vim.fn["codeium#Accept"]() end, expr = true, silent = true }
-        maps.i["<M-BS>"] = { function() return vim.fn["codeium#Clear"]() end, expr = true, silent = true }
+        maps.i["<M-[>"] = { function() require("neocodeium").cycle_or_complete(-1) end }
+        maps.i["<M-CR>"] = { function() require("neocodeium").accept() end }
+        maps.i["<M-BS>"] = { function() require("neocodeium").clear() end }
       end,
+    },
+    {
+      "rebelot/heirline.nvim",
+      opts = function(_, opts)
+        local status = require "astroui.status"
+
+        table.insert(
+          opts.statusline,
+          10,
+          status.component.builder {
+            {
+              provider = function()
+                local aistatus, serverstatus = require("neocodeium").get_status()
+                return status.utils.stylize(
+                  (aistatus ~= 0 and "󱚧") or (serverstatus == 0 and "󰚩" or "󱚢"),
+                  { padding = { right = 1 } }
+                )
+              end,
+            },
+            hl = { fg = "git_branch_fg" },
+            surround = {
+              separator = "right",
+              color = "section_bg",
+              condition = function() return vim.g.neocodeium_enabled end,
+            },
+          }
+        )
+      end,
+    },
+  },
+  opts = {
+    enabled = false,
+    manual = true,
+    silent = true,
+    filetypes = {
+      TelescopePrompt = false,
+      ["dap-repl"] = false,
     },
   },
 }
