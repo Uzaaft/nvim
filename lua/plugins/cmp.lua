42eefa47f136114df518aa66c9ce51deeb42de54
Micah Halter <micah@mehalter.com>
2024-10-17 08:35:44 -0400

    refactor(cmp): improve truncation function

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,6 +8,7 @@ return {
     "kdheepak/cmp-latex-symbols",
   },
   opts = function(_, opts)
+    local astrocore, astroui = require "astrocore", require "astroui"
     opts.sources = {
       {
         name = "nvim_lsp",
@@ -23,18 +24,21 @@ return {
       { name = "buffer", priority = 250, group_index = 2 },
     }
 
-    local format = opts.formatting.format
-    opts.formatting.format = function(entry, vim_item)
-      vim_item = format(entry, vim_item)
+    local truncate = function(str, len)
+      if not str then return end
+      local truncated = vim.fn.strcharpart(str, 0, len)
+      return truncated == str and str or truncated .. astroui.get_icon "Ellipsis"
+    end
 
-      -- truncate label
-      local max_width = math.floor(0.25 * vim.o.columns)
-      local label = vim_item.abbr
-      local truncated_label = vim.fn.strcharpart(label, 0, max_width)
-      if truncated_label ~= label then vim_item.abbr = truncated_label .. "…" end
+    if not opts.formatting then opts.formatting = {} end
+    opts.formatting.format = astrocore.patch_func(opts.formatting.format, function(format, ...)
+      local vim_item = format(...)
+
+      vim_item.abbr = truncate(vim_item.abbr, math.floor(0.25 * vim.o.columns))
+      vim_item.menu = truncate(vim_item.menu, math.floor(0.25 * vim.o.columns))
 
       return vim_item
-    end
+    end)
 
     if not opts.sorting then opts.sorting = {} end
     local compare = require "cmp.config.compare"
