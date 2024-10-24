e494db247dc29cfd653189e4272b5577b5f04e03
Micah Halter <micah@mehalter.com>
2024-10-24 09:47:04 -0400

    refactor(plugins): remove `magazine.nvim` plugin

--- lua/plugins/magazine.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "iguanacucumber/magazine.nvim",
-  lazy = true,
-  config = function() vim.opt.rtp:remove(require("astrocore").get_plugin("nvim-cmp").dir) end,
-  specs = { "hrsh7th/nvim-cmp", dependencies = { "iguanacucumber/magazine.nvim" } },
-}
