53105c9c8d8a6a9ac283f4717a1ad809593267a1
Micah Halter <micah@mehalter.com>
2024-11-20 16:13:04 -0500

    feat(plugins): add `avante.nvim`

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,72 @@
+return {
+  "yetone/avante.nvim",
+  build = "make",
+  -- version = "*",
+  cmd = {
+    "AvanteAsk",
+    "AvanteBuild",
+    "AvanteEdit",
+    "AvanteRefresh",
+    "AvanteSwitchProvider",
+    "AvanteChat",
+    "AvanteToggle",
+    "AvanteClear",
+  },
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = assert(opts.mappings)
+        local prefix = "<Leader>A"
+
+        maps.n[prefix] = { desc = "Avante functionalities" }
+
+        maps.n[prefix .. "a"] = { function() require("avante.api").ask() end, desc = "Avante ask" }
+        maps.v[prefix .. "a"] = { function() require("avante.api").ask() end, desc = "Avante ask" }
+
+        maps.v[prefix .. "r"] = { function() require("avante.api").refresh() end, desc = "Avante refresh" }
+
+        maps.n[prefix .. "e"] = { function() require("avante.api").edit() end, desc = "Avante edit" }
+        maps.v[prefix .. "e"] = { function() require("avante.api").edit() end, desc = "Avante edit" }
+
+        -- the following key bindings do not have an official api implementation
+        maps.n.co = { "<Plug>(AvanteConflictOurs)", desc = "Choose ours", expr = true }
+        maps.v.co = { "<Plug>(AvanteConflictOurs)", desc = "Choose ours", expr = true }
+
+        maps.n.ct = { "<Plug>(AvanteConflictTheirs)", desc = "Choose theirs", expr = true }
+        maps.v.ct = { "<Plug>(AvanteConflictTheirs)", desc = "Choose theirs", expr = true }
+
+        maps.n.ca = { "<Plug>(AvanteConflictAllTheirs)", desc = "Choose all theirs", expr = true }
+        maps.v.ca = { "<Plug>(AvanteConflictAllTheirs)", desc = "Choose all theirs", expr = true }
+
+        maps.n.cb = { "<Plug>(AvanteConflictBoth)", desc = "Choose both", expr = true }
+        maps.v.cb = { "<Plug>(AvanteConflictBoth)", desc = "Choose both", expr = true }
+
+        maps.n.cc = { "<Plug>(AvanteConflictCursor)", desc = "Choose cursor", expr = true }
+        maps.v.cc = { "<Plug>(AvanteConflictCursor)", desc = "Choose cursor", expr = true }
+
+        maps.n["]x"] = { "<Plug>(AvanteConflictPrevConflict)", desc = "Move to previous conflict", expr = true }
+
+        maps.n["[x"] = { "<Plug>(AvanteConflictNextConflict)", desc = "Move to next conflict", expr = true }
+      end,
+    },
+  },
+  opts = {
+    provider = "copilot",
+    auto_suggestions_provider = "copilot",
+  },
+  specs = {
+    { "zbirenbaum/copilot.lua", cmd = "Copilot", opts = {} },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if not opts.file_types then opts.file_types = { "markdown" } end
+        opts.file_types = require("astrocore").list_insert_unique(opts.file_types, { "Avante" })
+      end,
+    },
+  },
+}
