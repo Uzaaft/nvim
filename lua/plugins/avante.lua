55d430bd0ae5061de6cc78c02ebadc607f63ec6d
Micah Halter <micah@mehalter.com>
2024-11-20 16:13:04 -0500

    feat(plugins): add `avante.nvim`

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,83 @@
+return {
+  "yetone/avante.nvim",
+  build = "make",
+  -- version = "*",
+  cmd = {
+    "AvanteAsk",
+    "AvanteBuild",
+    "AvanteEdit",
+    "AvanteRefresh",
+    "AvanteSwitchProvider",
+    "AvanteChat",
+    "AvanteToggle",
+    "AvanteClear",
+  },
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = assert(opts.mappings)
+        local prefix = "<Leader>A"
+
+        maps.n[prefix] = { desc = " Avante" }
+
+        maps.n[prefix .. "<CR>"] = { function() require("avante.api").ask() end, desc = "Avante ask" }
+        maps.v[prefix .. "<CR>"] = { function() require("avante.api").ask() end, desc = "Avante ask" }
+
+        maps.v[prefix .. "r"] = { function() require("avante.api").refresh() end, desc = "Avante refresh" }
+
+        maps.n[prefix .. "e"] = { function() require("avante.api").edit() end, desc = "Avante edit" }
+        maps.v[prefix .. "e"] = { function() require("avante.api").edit() end, desc = "Avante edit" }
+      end,
+    },
+  },
+  opts = {
+    provider = "copilot",
+    auto_suggestions_provider = "copilot",
+    mappings = {
+      diff = {
+        ours = "co",
+        theirs = "ct",
+        all_theirs = "ca",
+        both = "cb",
+        cursor = "cc",
+        next = "]c",
+        prev = "[c",
+      },
+      suggestion = {
+        accept = "<M-l>",
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+      jump = {
+        next = "]]",
+        prev = "[[",
+      },
+      submit = {
+        normal = "<CR>",
+        insert = "<C-s>",
+      },
+      sidebar = {
+        apply_all = "A",
+        apply_cursor = "a",
+        switch_windows = "<Tab>",
+        reverse_switch_windows = "<S-Tab>",
+      },
+    },
+  },
+  specs = {
+    { "zbirenbaum/copilot.lua", cmd = "Copilot", opts = {} },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if not opts.file_types then opts.file_types = { "markdown" } end
+        opts.file_types = require("astrocore").list_insert_unique(opts.file_types, { "Avante" })
+      end,
+    },
+  },
+}
