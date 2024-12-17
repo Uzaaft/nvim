6ab8f866e225d93ee2ebaae0af41cb9b69956743
Micah Halter <micah.halter@gtri.gatech.edu>
2024-12-17 14:57:42 -0500

    feat: add vue-language-server configuration

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -138,8 +138,43 @@ return {
         },
       },
       typos_lsp = { single_file_support = false },
+      volar = { init_options = { vue = { hybridMode = true } } },
       vtsls = {
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+          "vue",
+        },
+
+        before_init = function(_, config)
+          local registry_ok, registry = pcall(require, "mason-registry")
+          if not registry_ok then return end
+          local vuels = registry.get_package "vue-language-server"
+
+          if vuels:is_installed() then
+            local volar_install_path = vuels:get_install_path() .. "/node_modules/@vue/language-server"
+
+            local vue_plugin_config = {
+              name = "@vue/typescript-plugin",
+              location = volar_install_path,
+              languages = { "vue" },
+              configNamespace = "typescript",
+              enableForWorkspaceTypeScriptVersions = true,
+            }
+
+            require("astrocore").list_insert_unique(config.settings.vtsls.tsserver.globalPlugins, { vue_plugin_config })
+          end
+        end,
         settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {},
+            },
+          },
           typescript = {
             inlayHints = {
               parameterNames = { enabled = "all", suppressWhenArgumentMatchesName = false },
