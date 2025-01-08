c7d07d8c061d35ae7be80a311a5e58dcedfd3fad
Micah Halter <micah.halter@gtri.gatech.edu>
2025-01-08 11:27:37 -0500

    fix(queries): remove markdown concealing

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -1,12 +1,2 @@
 ;; extends
 ((inline) @_inline (#match? @_inline "^\(import\|export\)")) @nospell
-
-; checkboxes
-((task_list_marker_unchecked) @markdown_check_undone (#set! conceal "󰄱"))
-((task_list_marker_checked) @markdown_check_done (#set! conceal "󰄵"))
-
-; box drawing characters for tables
-(pipe_table_header ("|") @punctuation.special @conceal (#set! conceal "│"))
-(pipe_table_delimiter_row ("|") @punctuation.special @conceal (#set! conceal "│"))
-(pipe_table_delimiter_cell ("-") @punctuation.special @conceal (#set! conceal "─"))
-(pipe_table_row ("|") @punctuation.special @conceal (#set! conceal "│"))
