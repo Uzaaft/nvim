a29a697343c023c9d0a3c028dbbc7b3e3e9bf5d8
Micah Halter <micah.halter@gtri.gatech.edu>
2025-03-04 09:11:37 -0500

    refactor(plugins): remove unnecessary disables

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,9 +1,6 @@
 local disabled = {
-  "goolord/alpha-nvim",
   "jay-babu/mason-null-ls.nvim",
-  "lukas-reineke/indent-blankline.nvim",
   "nvimtools/none-ls.nvim",
 }
-if vim.fn.has "nvim-0.10" == 1 then table.insert(disabled, "numToStr/Comment.nvim") end
 
 return vim.tbl_map(function(plugin) return { plugin, enabled = false } end, disabled)
