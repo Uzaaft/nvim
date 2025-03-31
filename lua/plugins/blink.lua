49aa155b2970bf628a5306d2a59836f6b596d8ca
Micah Halter <micah.halter@gtri.gatech.edu>
2025-03-31 08:32:29 -0400

    fix(blink): prefer snippet navigation with tab

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,6 +2,10 @@ return {
   "Saghen/blink.cmp",
   version = "^1",
   opts = {
+    keymap = {
+      ["<Tab>"] = { "accept", "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+    },
     sources = {
       providers = {
         path = { opts = { trailing_slash = false, show_hidden_files_by_default = true } },
