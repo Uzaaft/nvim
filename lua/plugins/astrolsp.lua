ec96bab8c7f5a35854b9cfa683846cffccdf700f
Micah Halter <micah.halter@gtri.gatech.edu>
2025-04-22 11:32:45 -0400

    chore: don't use native `vim.lsp.config`

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -10,7 +10,7 @@ return {
   "AstroNvim/astrolsp",
   ---@type AstroLSPOpts
   opts = {
-    native_lsp_config = true,
+    -- native_lsp_config = true,
     autocmds = {
       no_insert_inlay_hints = {
         cond = vim.lsp.inlay_hint and "textDocument/inlayHint" or false,
