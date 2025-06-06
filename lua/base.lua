05070efe6ea5ef61af390057a7751722612be6fe
Micah Halter <micah.halter@gtri.gatech.edu>
2025-06-06 11:11:16 -0400

    chore: move to AstroNvim v6 branch

--- lua/base.lua
+++ lua/base.lua
@@ -1,6 +1,6 @@
 ---@type LazySpec
 return {
-  { "AstroNvim/AstroNvim", branch = "astrolsp_v4", import = "astronvim.plugins" },
+  { "AstroNvim/AstroNvim", branch = "v6", import = "astronvim.plugins" },
   { "AstroNvim/astrolsp", branch = "v4" },
   -- { "AstroNvim/astrocommunity" },
   -- { import = "astrocommunity.pack.lua" },
